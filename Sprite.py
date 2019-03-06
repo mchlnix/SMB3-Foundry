f6a4ff5c5ee4101f5c2fcf09ded4dc3bffa740a2
Michael Nix <mchl.nix@googlemail.com>
2019-03-06 14:29:03 +0100

    adds Tile class

--- Sprite.py
+++ Sprite.py
@@ -1,9 +1,56 @@
 import wx
 
+TILE_MEMORY_OFFSET = 0x40010
+TILE_HEIGHT = 8
+TILE_WIDTH = 8
+TILE_PIXEL_COUNT = TILE_HEIGHT * TILE_WIDTH
+TILE_SIZE = int(TILE_PIXEL_COUNT * 2 / 8)  # pixel * pixel * 2 bit
+PIXEL_OFFSET = 8  # both bits describing the color of a pixel are in separate 8 byte chunks at the same index
 
-class Spritesheet(wx.Bitmap):
+DEFAULT_PALETTE = {
+    0b00: bytes([0xF7, 0xD8, 0xA5]),  # background
+    0b01: bytes([0xFE, 0xCC, 0xC5]),
+    0b10: bytes([0xB5, 0x31, 0x20]),
+    0b11: bytes([0x00, 0x00, 0x00]),
+}
+
+
+class Tile:
+    def __init__(self, rom, offset, palette=DEFAULT_PALETTE):
+        self.start = TILE_MEMORY_OFFSET + offset
+        self.palette = palette
+        self.data = bytearray()
+        self.pixels = bytearray()
+
+        rom.seek(self.start)
+
+        for _ in range(TILE_SIZE):
+            self.data.append(rom.get_byte())
+
+        for i in range(TILE_PIXEL_COUNT):
+            byte_index = i // 8
+            bit_index = 2 ** (i % 8)
+
+            left_bit = right_bit = 0
+
+            if self.data[byte_index] & bit_index: left_bit = 1
+            if self.data[PIXEL_OFFSET + byte_index] & bit_index: right_bit = 1
+
+            palette_value = (left_bit << 1) | right_bit
+
+            color = self.palette[palette_value]
+
+            self.pixels.extend(color)
+
+    def as_bitmap(self):
+        bitmap = wx.Bitmap.FromBuffer(TILE_WIDTH, TILE_HEIGHT, self.pixels)
+
+        return bitmap
+
+
+class SpriteSheet(wx.Bitmap):
     def __init__(self, source, sprite_width=16, sprite_height=16, *args, **kwargs):
-        super(Spritesheet, self).__init__(*args, **kwargs)
+        super(SpriteSheet, self).__init__(*args, **kwargs)
 
         self.source = wx.Bitmap(source)
         self.sprite_width = sprite_width
