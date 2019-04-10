3ee4eded079da04172f73f67c5eceaefdb2cb06a
Michael Nix <mchl.nix@googlemail.com>
2019-04-10 13:02:29 +0200

    adds PatternTable object

--- Graphics.py
+++ Graphics.py
@@ -39,6 +39,112 @@ ENDING_OBJECT_OFFSET = 0x1C8F9
 # not all objects provide a block index for blank block
 BLANK = -1
 
+graphic_set2chr_index = {
+    0: 0x00,   # not used
+    1: 0x08,   # Plains
+    2: 0x10,   # Fortress
+    3: 0x1C,   # Hills / Underground
+    4: 0x0C,   # Sky
+    5: 0x58,   # Piranha Plant
+    6: 0x58,   # Water
+    7: 0x5C,   # Mushroom
+    8: 0x58,   # Pipe
+    9: 0x30,   # Desert
+    10: 0x34,  # Ship
+    11: 0x6E,  # Giant
+    12: 0x18,  # Ice
+    13: 0x38,  # Cloudy
+    14: 0x1C,  # Not Used (same as 3)
+    15: 0x24,  # Bonus Room
+    16: 0x2C,  # Spade (Roulette)
+    17: 0x5C,  # N-Spade (Card)
+    18: 0x58,  # 2P vs.
+    19: 0x6C,  # Hills / Underground alternative
+    20: 0x68,  # 3-7 only
+    21: 0x34,  # World 8 War Vehicle
+    22: 0x28,  # Throne Room
+}
+
+common_set2chr_index = {
+    0: 0x00,   # not used
+    1: 0x60,   # Plains
+    2: 0x60,   # Fortress
+    3: 0x60,   # Hills / Underground
+    4: 0x60,   # Sky
+    5: 0x3E,   # Piranha Plant
+    6: 0x60,   # Water
+    7: 0x5E,   # Mushroom
+    8: 0x60,   # Pipe
+    9: 0x60,   # Desert
+    10: 0x6A,  # Ship
+    11: 0x60,  # Giant
+    12: 0x60,  # Ice
+    13: 0x60,  # Cloudy
+    14: 0x60,  # Not Used (same as 3)
+    15: 0x5E,  # Bonus Room
+    16: 0x2E,  # Spade (Roulette)
+    17: 0x5E,  # N-Spade (Card)
+    18: 0x60,  # 2P vs.
+    19: 0x60,  # Hills / Underground alternative
+    20: 0x60,  # 3-7 only
+    21: 0x70,  # World 8 War Vehicle
+    22: 0x60,  # Throne Room
+}
+
+
+CHR_ROM_OFFSET = 0x40010
+CHR_ROM_SEGMENT_SIZE = 0x400
+
+WORLD_MAP = 0
+SPADE_ROULETTE = 16
+N_SPADE = 17
+VS_2P = 18
+
+
+class PatternTable:
+    def __init__(self, arg):
+        self.data = bytearray()
+
+        if isinstance(arg, list):
+            self._readin(arg)
+        elif isinstance(arg, int):
+            self._from_graphic_set(arg)
+        else:
+            raise TypeError("Only list or int allowed.")
+
+    def _from_graphic_set(self, graphic_set):
+        segments = []
+
+        if graphic_set == WORLD_MAP:
+            segments = [0x14, 0x16, 0x20, 0x21, 0x22, 0x23]
+
+        gfx_index = graphic_set2chr_index[graphic_set]
+        common_index = common_set2chr_index[graphic_set]
+
+        segments.append(gfx_index)
+        segments.append(common_index)
+
+        if graphic_set == SPADE_ROULETTE:
+            segments.extend([0x20, 0x21, 0x22, 0x23])
+        elif graphic_set == N_SPADE:
+            segments.extend([0x28, 0x29, 0x5A, 0x31])
+        elif graphic_set == VS_2P:
+            segments.extend([0x04, 0x05, 0x06, 0x07])
+        else:
+            segments.extend([0x00, 0x00, 0x00, 0x00])
+
+        self._readin(segments)
+
+    def _readin(self, segments):
+        for segment in segments:
+            self._readin_chrrom_segment(segment)
+
+    def _readin_chrrom_segment(self, index):
+        offset = CHR_ROM_OFFSET + index * CHR_ROM_SEGMENT_SIZE
+        chr_rom_data = ROM().bulk_read(CHR_ROM_SEGMENT_SIZE, offset)
+
+        self.data.extend(chr_rom_data)
+
 
 class LevelObject:
     # todo better way of saving this information?
