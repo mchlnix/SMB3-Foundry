bb4e2f7d352ea2958608c7d4f49a90c72bd61498
Michael Nix <mchl.nix@googlemail.com>
2019-04-16 15:49:46 +0200

    format obj infos in status bar better

--- gui/ObjectStatusBar.py
+++ gui/ObjectStatusBar.py
@@ -1,6 +1,29 @@
 import wx
 
-from Graphics import LevelObject
+ENDING_STR = {
+    0: "Uniform",
+    1: "Top or Left",
+    2: "Bottom or Right",
+    3: "Top & Bottom/Left & Right"
+}
+
+ORIENTATION_TO_STR = {
+    0: "Horizontal",
+    1: "Vertical",
+    2: "Diagonal ↙",
+    3: "Desert Pipe Box",
+    4: "Diagonal ↘",
+    5: "Diagonal ↗",
+    6: "Horizontal to the Ground",
+    7: "Horizontal Alternative",
+    8: "Diagonal Weird",  # up left?
+    9: "Single Block",
+    10: "Centered",
+    11: "Pyramid to Ground",
+    12: "Pyramid Alternative",
+    13: "To the Sky",
+    14: "Ending"
+}
 
 
 class ObjectStatusBar(wx.StatusBar):
@@ -11,14 +34,14 @@ class ObjectStatusBar(wx.StatusBar):
         for i in range(self.GetFieldsCount()):
             self.SetStatusText("", i)
 
-    def fill(self, obj: LevelObject):
+    def fill(self, obj):
         self.SetFieldsCount(6)
 
         self.SetStatusText(f"x: {obj.rendered_base_x}", 0)
         self.SetStatusText(f"y: {obj.rendered_base_y}", 1)
 
-        self.SetStatusText(f"width: {obj.rendered_width}", 2)
-        self.SetStatusText(f"height: {obj.rendered_height}", 3)
+        self.SetStatusText(f"Width: {obj.rendered_width}", 2)
+        self.SetStatusText(f"Height: {obj.rendered_height}", 3)
 
-        self.SetStatusText(f"orientation: {obj.orientation}", 4)
-        self.SetStatusText(f"ends: {obj.ending}", 5)
+        self.SetStatusText(f"Orientation: {ORIENTATION_TO_STR[obj.orientation]}", 4)
+        self.SetStatusText(f"Ends: {ENDING_STR[obj.ending]}", 5)
