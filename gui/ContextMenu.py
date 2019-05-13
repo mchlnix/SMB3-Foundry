8c00e662f4ac827d59e39ac430d5268c580f994a
Michael Nix <mchl.nix@googlemail.com>
2019-05-13 16:37:26 +0200

    adds context menu version for object list

--- gui/ContextMenu.py
+++ gui/ContextMenu.py
@@ -7,6 +7,10 @@ ID_CTX_COPY = 704
 ID_CTX_PASTE = 705
 ID_CTX_CUT = 706
 
+MODE_BG = 0
+MODE_OBJ = 1
+MODE_LIST = 2
+
 
 class ContextMenu(wx.Menu):
     def __init__(self):
@@ -39,18 +43,27 @@ class ContextMenu(wx.Menu):
         return [item.GetId() for item in self.GetMenuItems()]
 
     def as_object_menu(self):
-        self._setup_items(True)
+        self._setup_items(MODE_OBJ)
 
         return self
 
     def as_background_menu(self):
-        self._setup_items(False)
+        self._setup_items(MODE_BG)
 
         return self
 
-    def _setup_items(self, as_object_menu):
-        self.FindItemById(ID_CTX_PASTE).Enable(bool(self.copied_objects))
+    def as_list_menu(self):
+        self._setup_items(MODE_LIST)
 
-        self.FindItemById(ID_CTX_COPY).Enable(as_object_menu)
-        self.FindItemById(ID_CTX_CUT).Enable(as_object_menu)
-        self.FindItemById(ID_CTX_REMOVE).Enable(as_object_menu)
+        return self
+
+    def _setup_items(self, mode):
+        self.FindItemById(ID_CTX_CUT).Enable(not mode == MODE_BG)
+        self.FindItemById(ID_CTX_COPY).Enable(not mode == MODE_BG)
+        self.FindItemById(ID_CTX_PASTE).Enable(
+            not mode == MODE_LIST and self.copied_objects
+        )
+
+        self.FindItemById(ID_CTX_REMOVE).Enable(not mode == MODE_BG)
+        self.FindItemById(ID_CTX_ADD_OBJECT).Enable(not mode == MODE_LIST)
+        self.FindItemById(ID_CTX_ADD_ENEMY).Enable(not mode == MODE_LIST)
