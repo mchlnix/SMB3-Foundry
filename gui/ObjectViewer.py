306b036213f4717582d6238bda4f83c50001f52b
Michael Nix <mchl.nix@googlemail.com>
2019-05-17 21:43:52 +0200

    fixes object viewer

--- gui/ObjectViewer.py
+++ gui/ObjectViewer.py
@@ -136,7 +136,7 @@ class ObjectDrawArea(wx.Panel):
     def __init__(self, parent, object_set, graphic_set=1, palette_index=0):
         super(ObjectDrawArea, self).__init__(parent)
 
-        self.object_factory = LevelObjectFactory(object_set, graphic_set, palette_index)
+        self.object_factory = LevelObjectFactory(object_set, graphic_set, palette_index, False)
 
         self.current_object = None
 
