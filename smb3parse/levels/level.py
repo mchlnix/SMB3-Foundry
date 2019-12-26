957a20d4ee67ee814a67e622966e23cd56b996ea
Michael Nix <mchl.nix@googlemail.com>
2019-12-27 00:26:19 +0100

    add level header to level class

--- smb3parse/levels/level.py
+++ smb3parse/levels/level.py
@@ -1,13 +1,16 @@
 from smb3parse.levels import HEADER_LENGTH
+from smb3parse.levels.level_header import LevelHeader
 from smb3parse.levels.world_map import WorldMap
 
 
 class Level:
     def __init__(self, rom, world: WorldMap, screen: int, row: int, column: int):
-        self.layout_address, self.enemy_address, self.object_set_index = world.level_for_position(screen, row, column)
+        self.layout_address, self.enemy_address, self.object_set_number = world.level_for_position(screen, row, column)
 
         self._rom = rom
 
         self.header_address = self.layout_address - HEADER_LENGTH
 
         self.header_bytes = self._rom.read(self.header_address, HEADER_LENGTH)
+
+        self.header = LevelHeader(self.header_bytes, self.object_set_number)
--- smb3parse/util.py
+++
@@ -1,4 +0,0 @@
-def little_endian(two_bytes: bytearray) -> int:
-    first, second = two_bytes
-
-    return (second << 8) + first
