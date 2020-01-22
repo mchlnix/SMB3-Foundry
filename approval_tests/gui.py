a76cda1cdd267b13f2be0d56c599a8951ccef806
Michael Nix <mchl.nix@googlemail.com>
2020-01-22 13:49:39 +0100

    adds approval test package for reference testing

---
+++ approval_tests/gui.py
@@ -0,0 +1,62 @@
+from typing import Union
+
+from PySide2.QtGui import QPixmap
+from PySide2.QtWidgets import QDialog, QDialogButtonBox, QHBoxLayout, QLabel, QVBoxLayout
+
+image_source = Union[QPixmap, str]
+
+
+def _get_pixmap_from_source(image: image_source) -> QPixmap:
+    if isinstance(image, str):
+        image = QPixmap(str)
+
+    return image
+
+
+class ApprovalDialog(QDialog):
+    def __init__(self, test_name: str, reference_image: QPixmap, generated_image: QPixmap):
+        super(ApprovalDialog, self).__init__()
+
+        self.setWindowTitle(test_name)
+
+        main_layout = QVBoxLayout(self)
+
+        self.image_layout = QHBoxLayout()
+
+        ref_image = QLabel()
+        ref_image.setPixmap(reference_image)
+
+        gen_image = QLabel()
+        gen_image.setPixmap(generated_image)
+
+        self.image_layout.addStretch()
+        self.image_layout.addWidget(ref_image)
+        self.image_layout.addWidget(gen_image)
+        self.image_layout.addStretch()
+
+        button_box = QDialogButtonBox()
+
+        button_box.addButton("Reject", QDialogButtonBox.RejectRole).clicked.connect(self.reject)
+        button_box.addButton("Accept", QDialogButtonBox.AcceptRole).clicked.connect(self.accept)
+
+        button_box.addButton("Accept as new Reference", QDialogButtonBox.ApplyRole).clicked.connect(self._on_overwrite)
+
+        main_layout.addLayout(self.image_layout)
+        main_layout.addWidget(button_box)
+
+    def _on_overwrite(self):
+        self.done(QDialogButtonBox.Apply)
+
+    @staticmethod
+    def compare(test_name: str, reference_image: image_source, generated_image: image_source):
+        reference_image = _get_pixmap_from_source(reference_image)
+        generated_image = _get_pixmap_from_source(generated_image)
+
+        if generated_image.toImage() == reference_image.toImage():
+            return QDialog.Accepted
+
+        dialog = ApprovalDialog(test_name, reference_image, generated_image)
+
+        dialog.exec_()
+
+        return dialog.result()
