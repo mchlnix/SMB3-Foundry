f97b9b1f7adbf7acb19f9c28b2e392e99be0a9c2
Michael Nix <mchl.nix@googlemail.com>
2020-01-26 18:43:05 +0100

    fix ending object in sky object set

--- foundry/game/gfx/objects/test_level_object.py
+++ foundry/game/gfx/objects/test_level_object.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import pytest
-from PySide2.QtGui import QImage, QPixmap
+from PySide2.QtGui import QPixmap
 
 from approval_tests.gui import ApprovalDialog
 from foundry.game.gfx.objects.LevelObjectFactory import LevelObjectFactory
@@ -10,7 +10,9 @@ from foundry.gui.ObjectViewer import ObjectDrawArea
 from smb3parse.objects.object_set import (
     HILLY_GRAPHICS_SET,
     HILLY_OBJECT_SET,
-    UNDERGROUND_GRAPHIC_SET,
+    PLAINS_GRAPHICS_SET,
+    PLAINS_OBJECT_SET,
+    UNDERGROUND_GRAPHICS_SET,
     UNDERGROUND_OBJECT_SET,
 )
 
@@ -73,7 +75,7 @@ def test_object_rendering_4_2(object_index, qtbot):
 
 @pytest.mark.parametrize("object_index", [0x80, 0x8F])  # 45 Degree Hill - Up/Right
 def test_object_rendering_8_1(object_index, qtbot):
-    object_factory = LevelObjectFactory(UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET, 0, [], False)
+    object_factory = LevelObjectFactory(UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET, 0, [], False)
 
     object_domain = 0x00
     level_object = object_factory.from_properties(object_domain, object_index, 0, 0, None, 0)
@@ -84,12 +86,12 @@ def test_object_rendering_8_1(object_index, qtbot):
 @pytest.mark.parametrize(
     "object_index, domain, object_set, graphic_set",
     [
-        (0x20, 0x00, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),  # 45 Degree Underground Hill - Down/Left
-        (0x2F, 0x00, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),
+        (0x20, 0x00, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),  # 45 Degree Underground Hill - Down/Left
+        (0x2F, 0x00, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),
         (0x60, 0x03, HILLY_OBJECT_SET, HILLY_GRAPHICS_SET),  # 30 Degree Hill - Down/Left
         (0x6F, 0x03, HILLY_OBJECT_SET, HILLY_GRAPHICS_SET),
-        (0xA0, 0x03, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),  # 30 Degree Underwater Hill - Down/Left
-        (0xAF, 0x03, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),
+        (0xA0, 0x03, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),  # 30 Degree Underwater Hill - Down/Left
+        (0xAF, 0x03, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),
     ],
 )
 def test_object_rendering_2_1(object_index, domain, object_set, graphic_set, qtbot):
@@ -103,8 +105,8 @@ def test_object_rendering_2_1(object_index, domain, object_set, graphic_set, qtb
 @pytest.mark.parametrize(
     "object_index, domain, object_set, graphic_set",
     [
-        (0x50, 0x04, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),  # Hilly Wall
-        (0x5F, 0x04, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHIC_SET),
+        (0x50, 0x04, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),  # Hilly Wall
+        (0x5F, 0x04, UNDERGROUND_OBJECT_SET, UNDERGROUND_GRAPHICS_SET),
     ],
 )
 def test_object_rendering_0_0(object_index, domain, object_set, graphic_set, qtbot):
@@ -113,3 +115,20 @@ def test_object_rendering_0_0(object_index, domain, object_set, graphic_set, qtb
     level_object = object_factory.from_properties(domain, object_index, 0, 0, 8, 0)
 
     _test_object_against_reference(level_object, qtbot)
+
+
+@pytest.mark.parametrize(
+    "object_set, graphics_set",
+    list(
+        zip(
+            range(PLAINS_OBJECT_SET, UNDERGROUND_OBJECT_SET + 1),
+            range(PLAINS_GRAPHICS_SET, UNDERGROUND_GRAPHICS_SET + 1),
+        )
+    ),
+)
+def test_ending_object(object_set, graphics_set, qtbot):
+    object_factory = LevelObjectFactory(object_set, graphics_set, 0, [], False)
+
+    ending_object = object_factory.from_properties(0x2, 0x09, 0, 0, None, 0)
+
+    _test_object_against_reference(ending_object, qtbot)
