9cde34c91ebc856fbc8fd34dc0c60143026e515d
Michael Nix <mchl.nix@googlemail.com>
2020-01-31 21:45:28 +0100

    moves all tests into separate directory

---
+++ smb3parse/tests/conftest.py
@@ -0,0 +1,28 @@
+from os import chdir
+from pathlib import Path
+
+import pytest
+
+from smb3parse.levels.world_map import WorldMap
+from smb3parse.util.rom import Rom
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cd_to_repo_root():
+    chdir(Path(__file__).parent.parent.parent)
+
+
+@pytest.fixture(scope="session")
+def rom():
+    with open("SMB3.nes", "rb") as rom_file:
+        yield Rom(bytearray(rom_file.read()))
+
+
+@pytest.fixture
+def world_1(rom):
+    return WorldMap.from_world_number(rom, 1)
+
+
+@pytest.fixture
+def world_8(rom):
+    return WorldMap.from_world_number(rom, 8)
