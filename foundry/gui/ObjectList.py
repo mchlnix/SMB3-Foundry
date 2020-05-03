de9642fa2e45b48dbe73fe5caa759f4603aee1a1
Michael Nix <mchl.nix@googlemail.com>
2020-05-03 18:43:01 +0200

    disables movement in object list again

--- foundry/gui/ObjectList.py
+++ foundry/gui/ObjectList.py
@@ -1,11 +1,8 @@
-from PySide2.QtCore import QMimeData, QTimer
 from PySide2.QtGui import QMouseEvent, QWindow, Qt
 from PySide2.QtWidgets import QListWidget, QSizePolicy
 
 from foundry.game.level.LevelRef import LevelRef
 from foundry.gui.ContextMenu import ContextMenu
-
-LEVEL_OBJECT_LIST_MIME_TYPE = "application/foundry"
 
 
 class ObjectList(QListWidget):
@@ -14,7 +11,6 @@ class ObjectList(QListWidget):
 
         self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
 
-        self.setDragDropMode(self.InternalMove)
         self.setSelectionMode(self.ExtendedSelection)
 
         self.level_ref: LevelRef = level_ref
@@ -23,12 +19,6 @@ class ObjectList(QListWidget):
         self.context_menu = context_menu
 
         self.itemSelectionChanged.connect(self.on_selection_changed)
-        self.model().rowsMoved.connect(self.on_objects_moved)
-
-        self.redraw_timer = QTimer(self)
-        self.redraw_timer.setSingleShot(True)
-
-        self.redraw_timer.timeout.connect(self.level_ref.data_changed.emit)
 
     def mousePressEvent(self, event: QMouseEvent):
         if event.button() == Qt.RightButton:
@@ -90,26 +80,6 @@ class ObjectList(QListWidget):
 
         if self.selectedIndexes():
             self.scrollTo(self.selectedIndexes()[-1])
-
-    def mimeData(self, items):
-        """
-        Since we have user data on the items, we need to implement this method, otherwise moving the items is
-        deactivated automatically. The actual data doesn't matter in our case, however.
-        """
-        mime_data = QMimeData()
-
-        mime_data.setData(LEVEL_OBJECT_LIST_MIME_TYPE, bytes())
-
-        return mime_data
-
-    def mimeTypes(self):
-        return super(ObjectList, self).mimeTypes() + [LEVEL_OBJECT_LIST_MIME_TYPE]
-
-    def on_objects_moved(self, _, start: int, end: int, __, target: int):
-        for index in range(end, start - 1, -1):
-            self.level_ref.level.objects.insert(target, self.level_ref.level.objects.pop(index))
-
-        self.redraw_timer.start(10)
 
     def selected_objects(self):
         return [self.item(index.row()).data(Qt.UserRole) for index in self.selectedIndexes()]
