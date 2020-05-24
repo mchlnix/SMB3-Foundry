ed405d29a0d89d2a766135f70e25f4b5038283c1
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-05-24 01:22:55 -0400

    Added basic math operations for Position
    Added basic math operations for Position.

--- foundry/game/Position.py
+++ foundry/game/Position.py
@@ -1,14 +1,39 @@
 from dataclasses import dataclass
 from PySide2.QtCore import QPoint
 
+
 @dataclass
 class Position:
-    x: int = 0
-    y: int = 0
+    _x: float = 0
+    _y: float = 0
+
+    @property
+    def x(self):
+        return int(self._x)
+
+    @x.setter
+    def x(self, pos):
+        self._x = pos
+
+    @property
+    def y(self):
+        return int(self._y)
+
+    @y.setter
+    def y(self, pos):
+        self._y = pos
+
+    def x_with_offset(self, offset: float):
+        """Provides an offset to the x position"""
+        return self.x + offset
+
+    def y_with_offset(self, offset: float):
+        """Provides an offset to the y position"""
+        return self.y + offset
 
     def to_qt(self) -> QPoint:
         """Returns the qpoint version of position"""
-        return QPoint(self.x, self.y)
+        return QPoint(int(self.x), int(self.y))
 
     def scale_to(self, scale_factor):
         """Scales both x and y by a scale factor"""
@@ -24,3 +49,78 @@ class Position:
     def scale_from(cls, pos, scale_factor):
         """Scales both x and y by a scale factor and creates a new object"""
         return cls(pos.x * scale_factor, pos.y * scale_factor)
+
+    def __add__(self, other):
+        """Adds both x and y together"""
+        if isinstance(other, Position):
+            return Position(self.x + other.x, self.y + other.y)
+        else:
+            return Position(self.x + other, self.y + other)
+
+    def __radd__(self, other):
+        """Adds both x and y together"""
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        """Subtract both x and y together"""
+        if isinstance(other, Position):
+            return Position(self.x - other.x, self.y - other.y)
+        else:
+            return Position(self.x - other, self.y - other)
+
+    def __rsub__(self, other):
+        """Subtract both x and y together"""
+        if isinstance(other, Position):
+            return Position(other.x - self.x, other.y - self.y)
+        else:
+            return Position(other - self.x, other - self.y)
+
+    def __mul__(self, other):
+        """Multiply both x and y together"""
+        if isinstance(other, Position):
+            return Position(self.x * other.x, self.y * other.y)
+        else:
+            return Position(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        """Multiply both x and y together"""
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        """Divide both x and y together"""
+        if isinstance(other, Position):
+            return Position(self.x / other.x, self.y / other.y)
+        else:
+            return Position(self.x / other, self.y / other)
+
+    def __rtruediv__(self, other):
+        """Divide both x and y together"""
+        if isinstance(other, Position):
+            return Position(other.x / self.x, other.y / self.y)
+        else:
+            return Position(other / self.x, other / self.y)
+
+    
+
+
+class LevelPosition(Position):
+    SCREEN_WIDTH = 0x10
+
+    @property
+    def rel_x(self):
+        """The x position in terms of the screen the position is in"""
+        return self.x % self.SCREEN_WIDTH
+
+    @property
+    def rel_x_inverse(self) -> int:
+        """The x position in terms of the screen from right to left"""
+        return self.SCREEN_WIDTH - self.rel_x
+
+    @property
+    def screen_pos(self) -> int:
+        """Provides a way to combine the x and y position, useful for recreating in-game logic"""
+        return self.x + (self.y * self.SCREEN_WIDTH)
+
+    @classmethod
+    def from_pos(cls, pos: Position):
+        return cls(pos.x, pos.y)
