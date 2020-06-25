904d12c6d1a5ae58dd2576b63cb9d0cd70dbaa91
Pieter <PiJoKra@users.noreply.github.com>
2020-06-25 11:14:53 +0000

    Add ability to select powerup for testing (#56)
    Add ability to choose powerup when testing level

---
+++ foundry/gui/HorizontalLine.py
@@ -0,0 +1,8 @@
+from PySide2.QtWidgets import QFrame
+
+# taken from https://stackoverflow.com/a/41068447/4252230
+class HorizontalLine(QFrame):
+    def __init__(self):
+        super(HorizontalLine, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
