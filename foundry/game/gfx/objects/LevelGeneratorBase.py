4570fa1fdbb84404ef277e445e9ec81af78910ac
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-07 03:49:38 -0400

    Added asm6 converting

--- foundry/game/gfx/objects/LevelGeneratorBase.py
+++ foundry/game/gfx/objects/LevelGeneratorBase.py
@@ -4,6 +4,7 @@ from yaml import CLoader as Loader
 from abc import ABC, abstractmethod
 
 from foundry import data_dir
+from smb3parse.asm6_converter import to_hex
 from foundry.game.Size import Size
 from foundry.game.Position import Position
 from foundry.game.level.LevelConstants import SCREEN_WIDTH, SCREEN_HEIGHT
@@ -99,6 +100,9 @@ class BlockGeneratorHandler:
         generator_definition = tileset << 11 + self.block_generator.generator_definition & 0b0111_1111_1111
         position, size = self.block_generator.position, self.block_generator.base_size
         self._block_generator = BlockGeneratorBase.generator_from_attributes(generator_definition, position, size)
+
+    def to_bytes(self) -> Tuple[int, bytearray]:
+        return self.block_generator.to_bytes()
 
 
 def get_object_definition_index_from_data(tileset: int, data: bytearray) -> int:
@@ -235,10 +239,24 @@ class BlockGeneratorBase(ABC):
         data[2] = index
         return BlockGeneratorBase.generator_from_bytes(tileset=tileset, data=data)
 
+    def get_properties(self) -> Tuple[int, int, int, int, int, int, int]:
+        x, y = self.position.x, self.position.y
+        width, height = self.base_size.width - 1, self.base_size.height - 1
+        domain, index = return_domain_and_index_from_generator(self.generator_definition)
+        tileset = get_tileset_from_index(self.generator_definition)
+        return tileset, domain, index, width, height, x, y
+
+    @abstractmethod
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+
     @abstractmethod
     def to_bytes(self) -> Tuple[int, bytearray]:
         """
-        Convert the block generator into the format it will be read in game
+        Convert the block generator into the format it will be read in game.
         :return: A tuple providing the supposed tileset and bytes provided in game.
         """
 
@@ -287,16 +305,23 @@ class BlockGeneratorBase(ABC):
 
 
 class SlopeGenerator(BlockGeneratorBase):
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+        tileset, domain, index, width, height, x, y = self.get_properties()
+        s = f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(max(min(x, 0xFF), 0))}, " \
+            f"{to_hex(index + width)} ; {self.generator.description}\n"
+        return s
+
     def to_bytes(self) -> Tuple[int, bytearray]:
         """
         Convert the block generator into the format it will be read in game
         :return: A tuple providing the supposed tileset and bytes provided in game.
         """
-        x, y = self.position.x, self.position.y
-        width, height = self.base_size.width - 1, self.base_size.height - 1
-        domain, index = return_domain_and_index_from_generator(self.generator_definition)
-        tileset = get_tileset_from_index(self.generator_definition)
-        return tileset, bytearray([domain << 5 + y, x, (index & 0xFFF0) + width])
+        tileset, domain, index, width, height, x, y = self.get_properties()
+        return tileset, bytearray([domain << 5 + y, x, (index & 0xF0) + width])
 
     def change_width(self, amount: int = 1) -> "BlockGeneratorBase":
         """
@@ -331,20 +356,32 @@ class SlopeGenerator(BlockGeneratorBase):
 
 
 class VerticalGenerator(BlockGeneratorBase):
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+        tileset, domain, index, width, height, x, y = self.get_properties()
+        if self.generator.bytes == 3:
+            return f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(x)}, " \
+                f"{to_hex(index + height)} ; {self.generator.description}\n"
+        elif self.generator.bytes == 4:
+            return f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(x)}, " \
+                f"{to_hex(index + width)}, {to_hex(height)} ; {self.generator.description}\n"
+        else:
+            return NotImplemented
+
     def to_bytes(self) -> Tuple[int, bytearray]:
         """
         Convert the block generator into the format it will be read in game
         :return: A tuple providing the supposed tileset and bytes provided in game.
         """
-        x, y = self.position.x, self.position.y
-        width, height = self.base_size.width - 1, self.base_size.height - 1
-        domain, index = return_domain_and_index_from_generator(self.generator_definition)
-        tileset = get_tileset_from_index(self.generator_definition)
+        tileset, domain, index, width, height, x, y = self.get_properties()
 
         if self.generator.bytes == 3:
-            return tileset, bytearray([domain << 5 + y, x, (index & 0xFFF0) + height])
+            return tileset, bytearray([domain << 5 + y, x, (index & 0xF0) + height])
         elif self.generator.bytes == 4:
-            return tileset, bytearray([domain << 5 + y, x, (index & 0xFFF0) + width, height])
+            return tileset, bytearray([domain << 5 + y, x, (index & 0xF0) + width, height])
         else:
             return NotImplemented
 
@@ -401,21 +438,32 @@ class VerticalGenerator(BlockGeneratorBase):
 
 
 class HorizontalGenerator(BlockGeneratorBase):
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+        tileset, domain, index, width, height, x, y = self.get_properties()
+        if self.generator.bytes == 3:
+            return f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(x)}, " \
+                f"{to_hex(index + width)} ; {self.generator.description}\n"
+        elif self.generator.bytes == 4:
+            return f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(x)}, " \
+                f"{to_hex(index + height)}, {to_hex(width)} ; {self.generator.description}\n"
+        else:
+            return NotImplemented
+
     def to_bytes(self) -> Tuple[int, bytearray]:
         """
         Convert the block generator into the format it will be read in game
         :return: A tuple providing the supposed tileset and bytes provided in game.
         """
-        x, y = self.position.x, self.position.y
-        width, height = self.base_size.width - 1, self.base_size.height - 1
-        domain, index = return_domain_and_index_from_generator(self.generator_definition)
-        tileset = get_tileset_from_index(self.generator_definition)
-
+        tileset, domain, index, width, height, x, y = self.get_properties()
         gen = get_generator_from_index(self.generator_definition)
         if gen.bytes == 3:
-            return tileset, bytearray([domain << 5 + y, x, (index & 0xFFF0) + width])
+            return tileset, bytearray([domain << 5 + y, x, (index & 0xF0) + width])
         elif gen.bytes == 4:
-            return tileset, bytearray([domain << 5 + y, x, (index & 0xFFF0) + height, width])
+            return tileset, bytearray([domain << 5 + y, x, (index & 0xF0) + height, width])
         else:
             return NotImplemented
 
@@ -474,14 +522,21 @@ class HorizontalGenerator(BlockGeneratorBase):
 
 
 class SimpleGenerator(BlockGeneratorBase):
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+        tileset, domain, index, width, height, x, y = self.get_properties()
+        return f"\t.byte {to_hex(domain << 5)} | {to_hex(y)}, {to_hex(x)}, " \
+            f"{to_hex(index)} ; {self.generator.description}\n"
+
     def to_bytes(self) -> Tuple[int, bytearray]:
         """
         Convert the block generator into the format it will be read in game
         :return: A tuple providing the supposed tileset and bytes provided in game.
         """
-        x, y = self.position.x, self.position.y
-        domain, index = return_domain_and_index_from_generator(self.generator_definition)
-        tileset = get_tileset_from_index(self.generator_definition)
+        tileset, domain, index, width, height, x, y = self.get_properties()
         return tileset, bytearray([domain << 5 + y, x, index])
 
     def change_width(self, amount: int = 1) -> "BlockGeneratorBase":
@@ -502,6 +557,53 @@ class SimpleGenerator(BlockGeneratorBase):
 
     @classmethod
     def from_bytes(cls, tileset: int, data: bytearray) -> "SimpleGenerator":
+        """
+        Convert data into in readable data
+        :param tileset: The tileset for the generator
+        :param data: The data in game
+        :return: A block generator
+        """
+        generator_index = get_object_definition_index_from_data(tileset=tileset, data=data)
+        position = Position(data[1], data[0] & 0b0001_1111)
+        size = Size(1, 1)
+        return cls(generator_definition=generator_index, position=position, base_size=size)
+
+
+class CrashGenerator(BlockGeneratorBase):
+    def to_asm6(self) -> str:
+        """
+        Convert the block generator into compiler ready format.
+        :return: A string for 6502, asm6, format.
+        """
+        return ""
+
+    """Effectively removes the object from existing"""
+    def to_bytes(self) -> Tuple[int, bytearray]:
+        """
+        Convert the block generator into the format it will be read in game
+        :return: A tuple providing the supposed tileset and bytes provided in game.
+        """
+        tileset = get_tileset_from_index(self.generator_definition)
+        return tileset, bytearray()
+
+    def change_width(self, amount: int = 1) -> "BlockGeneratorBase":
+        """
+        Provides a new block generator with the new size
+        :param amount: The amount to be expanded horizontally
+        :return: A new block generator
+        """
+        return self
+
+    def change_height(self, amount: int = 1) -> "BlockGeneratorBase":
+        """
+        Provides a new block generator with the new size
+        :param amount: The amount to be expanded vertically
+        :return: A new block generator
+        """
+        return self
+
+    @classmethod
+    def from_bytes(cls, tileset: int, data: bytearray) -> "CrashGenerator":
         """
         Convert data into in readable data
         :param tileset: The tileset for the generator
