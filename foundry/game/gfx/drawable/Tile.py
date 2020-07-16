3e514dd4cb34460fbc5809079ea89652c69d903b
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-16 11:44:54 -0400

    Added documentation

--- foundry/game/gfx/drawable/Tile.py
+++ foundry/game/gfx/drawable/Tile.py
@@ -1,7 +1,16 @@
+"""
+This module converts the Nintendo's 2 bit per pixel image format into a Tile or a 8x8 RGB image in bytes.
+get_byte_bits: Converts a byte to a row 1 bit per pixel row of image data
+get_tile_row: Converts the upper and lower 1 bit per pixel row into a 2 bit per pixel representation
+get_color: A color lookup that allows for transparency
+qimage_mask: Masks out a color and returns a alpha mask
+Tile: The class for loading any NES graphic
+"""
+
 from typing import List
 from operator import add
-from functools import cached_property, lru_cache
-from PySide2.QtCore import QPoint
+from functools import cached_property
+from methodtools import lru_cache  # functools has a similar lru_cache, but can leak memory and is less class friendly
 from PySide2.QtGui import QImage, QPainter, Qt, QColor, QPixmap
 import numpy as np
 
@@ -18,7 +27,14 @@ BACKGROUND_COLOR_INDEX = 0
 
 @lru_cache
 def get_byte_bits(b: int, reverse: bool = False, false_value: int = 0, true_value: int = 1) -> List[int]:
-    """Returns a bit array of True and False values in terms of a specific int"""
+    """
+    Converts a byte to a row 1 bit per pixel row of image data
+    :param b: The byte to be converted
+    :param reverse: To start from the left or right respectively
+    :param false_value: The value of the bit if False
+    :param true_value: The value of the bit if True
+    :return: A list representation of a bit
+    """
     if reverse:
         return [true_value if b & (0x80 >> i) else false_value for i in range(8)]
     else:
@@ -26,12 +42,22 @@ def get_byte_bits(b: int, reverse: bool = False, false_value: int = 0, true_valu
 
 
 def get_tile_row(byte_1: int, byte_2: int) -> map:
-    """Returns a map of a tile row"""
+    """
+    Converts the upper and lower 1 bit per pixel row into a 2 bit per pixel representation
+    :param byte_1: The lower byte to be converted
+    :param byte_2: The upper byte to be converted
+    :return: A list representation of the NES 2bpp format
+    """
     return map(add, get_byte_bits(byte_1, True), get_byte_bits(byte_2, True, true_value=2))
 
 
 def get_color(b: int, palette):
-    """Gets the appropriate color for a given pixel and palette"""
+    """
+    A color lookup that allows for transparency
+    :param b: The idx in terms of the NES palette
+    :param palette: The NES palette to look up
+    :return: The correct NES color
+    """
     # add alpha values
     if b == BACKGROUND_COLOR_INDEX:
         return MASK_COLOR
@@ -39,8 +65,19 @@ def get_color(b: int, palette):
         return NESPalette[palette[b]]
 
 
+def qimage_mask(image: QImage) -> QImage:
+    """
+    Makes a mask from a given image
+    :param image: The QImage to be masked
+    :return: A QImage of the given alpha mask
+    """
+    return image.createMaskFromColor(QColor(*MASK_COLOR).rgb(), Qt.MaskOutColor)
+
+
 class Tile:
-    """A single 8x8 tile in game"""
+    """
+    A square that represents the fundamental unit of the Nintendo's 2bpp image format.
+    """
     image: QPixmap
 
     image_length = 8
@@ -54,7 +91,13 @@ class Tile:
 
     @classmethod
     def from_palette_and_pixels(cls, plane_1: List[int], plane_2: List[int], palette):
-        """Returns a tiles pixels"""
+        """
+        Converts from the Nintendo's 2 bit per pixel format into RGB pixels
+        :param plane_1: The lower plane of bytes
+        :param plane_2: The upper plane of bytes
+        :param palette: The palette for the Tile
+        :return: Tile
+        """
         pixels = bytearray()
         for (byte_1, byte_2) in zip(plane_1, plane_2):
             for pixel in get_tile_row(byte_1, byte_2):
@@ -62,12 +105,19 @@ class Tile:
         return cls(bytes(pixels))
 
     @classmethod
-    def from_rom(cls, object_index: int, palette_group: List[List[int]], palette_index: int, graphics_set: GraphicsPage):
-        """Returns a tile directly from rom"""
+    def from_rom(cls, object_index: int, palette_group: List[List[int]], palette_index: int, graphics_page: GraphicsPage):
+        """
+        Makes a Tile directly from the ROM
+        :param object_index: The index into the graphics page (0 - 0xFF)
+        :param palette_group: The palettes available
+        :param palette_index: The palette used
+        :param graphics_page: The page (upper) index into the graphics
+        :return: Tile
+        """
         start = object_index * 0x10
         palette = palette_group[palette_index]
         return cls.from_palette_and_pixels(
-            graphics_set.data[start: start + 0x08], graphics_set.data[start + 0x08: start + 0x10], palette
+            graphics_page.data[start: start + 0x08], graphics_page.data[start + 0x08: start + 0x10], palette
         )
 
     @property
@@ -83,46 +133,91 @@ class Tile:
     @lru_cache
     def qimage_custom(self, width: int = image_length, height: int = image_length, horizontal_mirror: bool = False,
                       vertical_mirror: bool = False, transparent: bool = False) -> QImage:
-        """A sized qimage"""
+        """
+        Provides a customized QImage from the Tile
+        :param width: The width of the QImage
+        :param height: The height of the QImage
+        :param horizontal_mirror: Determines if the QImage will be flipped over the y axis
+        :param vertical_mirror: Determines if the QImage will be flipped over the x axis
+        :param transparent: Determines if the Tile will mask out transparency
+        :return: QImage
+        """
         image = self.qimage_transparent if transparent else self.qimage
         return image.mirrored(horizontal_mirror, vertical_mirror).scaled(width, height)
 
     @cached_property
     def qimage_transparent(self) -> QImage:
-        """Returns a qimage with an alpha channel"""
+        """
+        Applies a mask to the QImage constructor to make it transparent
+        :return: An RGBA QImage
+        """
         image = self.qimage
-        mask = self.qimage_mask(image=image)
-        image.setAlphaChannel(mask)
+        image.setAlphaChannel(qimage_mask(image=image))
         return image
 
-    def qimage_mask(self, image: QImage) -> QImage:
-        """Makes a mask of the image"""
-        return image.createMaskFromColor(QColor(*MASK_COLOR).rgb(), Qt.MaskOutColor)
 
     @cached_property
     def qimage(self) -> QImage:
-        """Returns a qimage"""
+        """
+        Makes a RGB QImage of the Tile
+        :return: QImage
+        """
         image = QImage(self.pixels, self.default_size.width, self.default_size.height, QImage.Format_RGB888)
         return image
 
     @lru_cache
     def qpixmap_custom(self, width: int = image_length, height: int = image_length, horizontal_mirror: bool = False,
                        vertical_mirror: bool = False, transparent: bool = True) -> QPixmap:
-        """A sized qpixmap"""
+        """
+        Provides a customized QPixmap from the Tile
+        :param width: The width of the QPixmap
+        :param height: The height of the QPixmap
+        :param horizontal_mirror: Determines if the QPixmap will be flipped over the y axis
+        :param vertical_mirror: Determines if the QPixmap will be flipped over the x axis
+        :param transparent: Determines if the Tile will mask out transparency
+        :return: QPixmap
+        """
         return QPixmap.fromImage(self.qimage_custom(width, height, horizontal_mirror, vertical_mirror, transparent))
 
     @cached_property
     def qpixmap(self) -> QPixmap:
-        """Returns a qpixmap"""
+        """
+        Makes a RGB QPixmap of the Tile
+        :return: QPixmap
+        """
         return QPixmap.fromImage(self.qimage)
 
     @cached_property
-    def numpy_image(self) -> np.array:
-        """Returns a 2d numpy array of the object"""
-        return np.reshape(np.asarray(bytearray(self.pixels), dtype="ubyte"), (self.default_size.width, 3*self.default_size.height))
+    def numpy_image(self) -> np.array[np.dtype: np.ubyte]:
+        """
+        Makes a 2D virtualization of the data in terms of a numpy array
+        :return: np.array[dtype: byte, shape(Tile.default_size.width, Tile.default_size.height * 3)]
+        """
+        return np.reshape(
+            np.asarray(bytearray(self.pixels), dtype="ubyte"), (self.default_size.width, 3 * self.default_size.height)
+        )
 
-    def draw(self, painter: QPainter, pos: Position, size: Size, horizontal_mirror: bool = False,
-             vertical_mirror: bool = False, transparent: bool = False):
-        """Draws the image onto the screen"""
-        pixmap = self.qpixmap_custom(size.width, size.height, horizontal_mirror, vertical_mirror, transparent)
-        painter.drawPixmap(pos.x, pos.y, pixmap)
+    def draw(
+        self,
+        painter: QPainter,
+        pos: Position,
+        size: Size,
+        horizontal_mirror: bool = False,
+        vertical_mirror: bool = False,
+        transparent: bool = False
+    ) -> None:
+        """
+        Draws the Tile with a QPainter
+        :param painter: The QPainter that will draw the image
+        :param pos: The position to draw the image
+        :param size: The size of the image to draw
+        :param horizontal_mirror: Flips the image over the y axis
+        :param vertical_mirror: Flips the image over the x axis
+        :param transparent: Masks out the background color
+        :return: None
+        """
+        painter.drawPixmap(
+            pos.x,
+            pos.y,
+            self.qpixmap_custom(size.width, size.height, horizontal_mirror, vertical_mirror, transparent)
+        )
