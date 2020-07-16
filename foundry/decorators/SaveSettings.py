d1ec93b7e8c214ee3df971130cfea7db07d4c113
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-16 11:42:33 -0400

    Added decorator to help with loading and saving settings

---
+++ foundry/decorators/SaveSettings.py
@@ -0,0 +1,20 @@
+"""
+Provides a context decorator to help with saving and loading settings
+"""
+
+from typing import Callable
+from functools import wraps
+
+from foundry.gui.settings import load_settings, save_settings
+
+
+def handle_settings(func: Callable) -> Callable:
+    """Returns a function that will load and save the settings"""
+    @wraps
+    def inner(*args, **kwargs):
+        """Loads the settings, runs the function, and saves the settings"""
+        load_settings()
+        result = func(args, kwargs)
+        save_settings()
+        return result
+    return inner
