0191ea9d68878a67d913911ad294c057c78b56cf
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-20 11:26:11 -0400

    Added File Menu Light

--- foundry/gui/QMenus/FileMenu.py
+++ foundry/gui/QMenus/FileMenu.py
@@ -36,6 +36,25 @@ class FileMenu(Menu):
         self.add_action(self.exit_action.name, self.exit_action.action)
 
 
+class FileMenuLight(Menu):
+    """A menu for loading files"""
+    def __init__(self, parent):
+        super().__init__(parent, "File")
+        self.parent = parent
+
+        self.open_rom_action = OpenRomMenuElement(self)
+        self.addSeparator()
+        self.save_rom_action = SaveROMMenuElement(self)
+        self.save_rom_as_action = SaveROMasMenuElement(self)
+        self.save_asm6_action = SaveASM6MenuElement(self)
+        self.addSeparator()
+        self.settings_action = SettingsMenuElement(self.parent, False)
+        self.add_action(self.settings_action.name, self.settings_action.action)
+        self.addSeparator()
+        self.exit_action = ExitMenuElement(self.parent, False)
+        self.add_action(self.exit_action.name, self.exit_action.action)
+
+
 class ExitMenuElement(MenuElement):
     """A menu element that handles exiting the window"""
     @property
