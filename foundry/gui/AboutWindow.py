1031ba5b995101b1315ced5b2231b0793f015a9c
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-20 11:24:05 -0400

    Updated About Window

--- foundry/gui/AboutWindow.py
+++ foundry/gui/AboutWindow.py
@@ -4,12 +4,17 @@ from PySide2.QtWidgets import QBoxLayout, QFrame, QLabel
 from foundry import data_dir, get_current_version_name
 from foundry.gui.CustomDialog import CustomDialog
 
-LINK_SMB3F = "https://github.com/mchlnix/SMB3-Foundry"
+LINK_SMB3_FOUNDRY = "https://github.com/mchlnix/SMB3-Foundry"
+LINK_SMB3_PRIME = "https://smb3p.kafuka.org/index.php"
+LINK_SMB3_WIKI = "https://www.smb3prime.org/wiki/Main_Page"
 LINK_HUKKA = "http://hukka.ncn.fi/index.php?about"
-LINK_SMB3WS = "https://www.romhacking.net/utilities/298/"
+LINK_SMB3WORKSHOP = "https://www.romhacking.net/utilities/298/"
 LINK_SOUTHBIRD = "https://github.com/captainsouthbird"
 LINK_DISASM = "https://github.com/captainsouthbird/smb3"
 LINK_BLUEFINCH = "https://www.twitch.tv/bluefinch3000"
+LINK_JOE_SMO = "https://github.com/TheJoeSmo"
+LINK_PIJOKRA = "https://github.com/PiJoKra"
+LINK_MICHAEL = "https://github.com/mchlnix"
 
 
 class AboutDialog(CustomDialog):
@@ -29,24 +34,23 @@ class AboutDialog(CustomDialog):
 
         text_layout.addWidget(QLabel(f"SMB3 Foundry v{get_current_version_name()}", self))
         text_layout.addWidget(QHLine())
-        text_layout.addWidget(LinkLabel(self, f'By <a href="{LINK_SMB3F}">Michael</a>'))
-        text_layout.addWidget((QLabel("", self)))
-        text_layout.addWidget(QLabel("With thanks to:", self))
-        text_layout.addWidget(
-            LinkLabel(self, f'<a href="{LINK_HUKKA}">Hukka</a> for <a href="{LINK_SMB3WS}">SMB3 Workshop</a>')
-        )
-        text_layout.addWidget(
-            LinkLabel(
-                self,
-                f'<a href="{LINK_SOUTHBIRD}">Captain Southbird</a> for the <a href="{LINK_DISASM}">SMB3 Disassembly</a>',
-            )
-        )
-        text_layout.addWidget(
-            LinkLabel(
-                self,
-                f'<a href="{LINK_BLUEFINCH}">BlueFinch</a>, ZacMario and SKJyannick for testing and sanity checking',
-            )
-        )
+        text_layout.addWidget(LinkLabel(self, f'Authors:'))
+        text_layout.addWidget(LinkLabel(self, f'  • <a href="{LINK_MICHAEL}">Michael</a>      '
+                                              f'  • <a href="{LINK_JOE_SMO}">Joe Smo</a>      '
+                                              f'  • <a href="{LINK_PIJOKRA}">PiJoKra</a>'))
+        text_layout.addWidget(LinkLabel(self, f''))
+        text_layout.addWidget(LinkLabel(self, f'Testers: '))
+        text_layout.addWidget(LinkLabel(self, f'  • <a href="{LINK_BLUEFINCH}">BlueFinch</a>  '
+                                              f'  • ZacMario  '
+                                              f'  • SKJyannick'))
+        text_layout.addWidget(LinkLabel(self, f''))
+        text_layout.addWidget(LinkLabel(self, f'Special Thanks To:'))
+        text_layout.addWidget(LinkLabel(self, f'  • <a href="{LINK_HUKKA}">Hukka</a> for our predecessor '
+                                              f'<a href="{LINK_SMB3WORKSHOP}">SMB3 Workshop</a>'))
+        text_layout.addWidget(LinkLabel(self, f'  • <a href="{LINK_SOUTHBIRD}">Captain Southbird</a> '
+                                              f'for the tremendous work on the '
+                                              f'<a href="{LINK_DISASM}">SMB3 Disassembly</a>'))
+        text_layout.addWidget(LinkLabel(self, f'  • And most importantly you!'))
 
         main_layout.addLayout(text_layout)
 
