9d1135cc6ec5d40b07a3aa474fee3434b7fc4a3e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-22 17:49:12 -0400

    Created a default toolbox

---
+++ foundry/gui/QToolbar/__init__.py
@@ -0,0 +1,21 @@
+from PySide2.QtWidgets import QSizePolicy, QToolBar, QWidget
+from PySide2.QtGui import Qt
+
+
+class Toolbar(QToolBar):
+    """Our custom toolbar to provide consistent functionality"""
+    @classmethod
+    def default_toolbox(cls, parent: QWidget, name: str, widget: QWidget, side: int) -> "Toolbar":
+        """Makes a default toolbox"""
+        toolbar = QToolBar(name, parent)
+        toolbar.setContextMenuPolicy(Qt.PreventContextMenu)
+        toolbar.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        toolbar.setOrientation(Qt.Horizontal)
+        toolbar.setFloatable(True)
+        toolbar.toggleViewAction().setChecked(True)
+        toolbar.toggleViewAction().trigger()
+        toolbar.addWidget(widget)
+        toolbar.setAllowedAreas(Qt.LeftToolBarArea | Qt.RightToolBarArea | Qt.TopToolBarArea | Qt.BottomToolBarArea)
+        parent.addToolBar(side)
+        return toolbar
+    
