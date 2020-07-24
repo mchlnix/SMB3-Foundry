fd52d047a04f5c17108d3beaf8adfb6b2b86a4c2
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-23 22:11:45 -0400

    Added label widget

---
+++ foundry/gui/QLabel/__init__.py
@@ -0,0 +1,17 @@
+"""
+This module includes all the different Labels for including text
+"""
+
+from typing import Callable, Optional
+from PySide2.QtWidgets import QWidget, QLabel, QSizePolicy
+from PySide2.QtGui import Qt
+
+
+class Label(QLabel):
+    """A generic spinner with extended functionality"""
+    def __init__(self, parent: Optional[QWidget], text: str) -> None:
+        super().__init__(parent=parent)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setFixedHeight(self.sizeHint().toTuple()[1] + 6)
+        self.setText(text)
+
