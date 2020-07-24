9ddeb441bbc370f24afe5bd33bf645dc2123c4a0
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-23 22:23:09 -0400

    Added more label sizes

--- foundry/gui/QCore/__init__.py
+++ foundry/gui/QCore/__init__.py
@@ -6,3 +6,5 @@ MARGIN_TIGHT = 2
 
 LABEL_TINY = 60  # default super small size for label
 LABEL_TIGHT = 80  # default small size for label
+LABEL_MEDIUM = 100  # default medium size for a label
+LABEL_LARGE = 120  # default large size for a label
