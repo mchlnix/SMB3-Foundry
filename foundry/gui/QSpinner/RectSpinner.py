6b63c2604148ae1ee90e67aec93df7f327ec897d
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-23 22:13:02 -0400

    Added RectSpinner

---
+++ foundry/gui/QSpinner/RectSpinner.py
@@ -0,0 +1,62 @@
+"""
+This module includes the RectSpinner
+RectSpinner: A spinner that automatically keeps tracks of a Rect.
+the_rect: The rect provided from the RectSpinner
+"""
+
+from typing import Optional
+from PySide2.QtWidgets import QWidget, QSizePolicy, QVBoxLayout, QLayout
+
+from foundry.gui.QLabel import Label
+from foundry.decorators.Observer import Observed
+from foundry.gui.QSpinner.MultiSpinner import MultiSpinnerPanel
+from foundry.gui.QSpinner.PositionSpinner import PositionSpinner
+from foundry.gui.QSpinner.SizeSpinner import SizeSpinner
+from foundry.gui.QCore import LABEL_TIGHT
+from foundry.game.Rect import Rect
+
+
+class RectSpinner(QWidget):
+    """A class for keeping track of a rect"""
+    def __init__(self, parent: Optional[QWidget], name: str, rect: Rect = Rect(6, 0, 1, 0)) -> None:
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.setSizeConstraint(QLayout.SetFixedSize)
+
+        self.label = Label(self, name)
+        self.label.setFixedWidth(LABEL_TIGHT)
+        layout.addWidget(self.label)
+        self.position_spinner = MultiSpinnerPanel(self, "Position", PositionSpinner(self))
+        layout.addWidget(self.position_spinner)
+        self.size_spinner = MultiSpinnerPanel(self, "Size", SizeSpinner(self))
+        layout.addWidget(self.size_spinner)
+
+        self.setLayout(layout)
+
+        self.action = Observed(
+            lambda *_: Rect.from_size_and_position(
+                self.size_spinner.spinner.the_size, self.position_spinner.spinner.position
+            )
+        )
+        self.the_rect = rect
+        self.size_spinner.add_observer(self.action)
+        self.position_spinner.add_observer(self.action)
+        self.action.attach(lambda rec: self.__setattr__("_rect", rec))
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.parent}, {self.name}, {self.the_rect})"
+
+    @property
+    def the_rect(self) -> Rect:
+        """The rect we are keeping track of"""
+        return self._rect
+
+    @the_rect.setter
+    def the_rect(self, rect: Rect) -> None:
+        self._rect = rect
+        self.position_spinner.spinner.position = rect.abs_pos
+        self.size_spinner.spinner.the_size = rect.abs_size
+        self.action.notify(rect)
