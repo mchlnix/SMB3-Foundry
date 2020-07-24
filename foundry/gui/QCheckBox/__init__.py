d20075ee53908123de2482c975f95131403f0f2e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-23 22:11:37 -0400

    Added CheckBox widget

---
+++ foundry/gui/QCheckBox/__init__.py
@@ -0,0 +1,40 @@
+
+
+from typing import Callable, Optional
+from PySide2.QtWidgets import QCheckBox, QFormLayout, QWidget, QSizePolicy
+
+from foundry.gui.QLabel import Label
+from foundry.decorators.Observer import Observed
+
+
+class CheckBox(QCheckBox):
+    """A generic spinner with extended functionality"""
+    def __init__(self, parent, name):
+        super().__init__(parent=parent, text=name)
+        self.action = Observed(self.action)
+        self.stateChanged.connect(self.action)
+
+    def add_observer(self, observer: Callable) -> None:
+        """Adds an observer to the value"""
+        self.aciton.attach_observer(observer)
+
+    def action(self, new_value: int, *_) -> int:
+        """
+        Extends the connect functionality from Qt
+        """
+        return new_value
+
+
+class CheckboxPanel(QWidget):
+    """A spinner panel with a basic form layout"""
+    def __init__(self, parent: Optional[QWidget], name: str, checkbox: CheckBox):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+        self.parent = parent
+        self.checkbox = checkbox
+        self.action = self.checkbox.action
+        self.add_observer = self.checkbox.add_observer
+        spinner_layout = QFormLayout()
+        spinner_layout.addRow(self.checkbox, Label(self.parent, name))
+        self.setLayout(spinner_layout)
