c559e67a074771cc6b83a21a4e64b409927caca2
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-23 22:07:59 -0400

    Created QCore

---
+++ foundry/gui/QCore/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains global variables used to make gui
+"""
+
+MARGIN_TIGHT = 2
+
+LABEL_TINY = 60  # default super small size for label
+LABEL_TIGHT = 80  # default small size for label
