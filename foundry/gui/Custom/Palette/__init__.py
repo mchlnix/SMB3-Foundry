dcda99bac7fc14ddf349756faeca4eb05b39a79c
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-24 23:23:08 -0400

    Added palette editors

--- foundry/gui/Custom/Palette/__init__.py
+++ foundry/gui/Custom/Palette/__init__.py
@@ -16,6 +16,81 @@ _default_palette = Palette(_default_color, _default_color, _default_color, _defa
 _default_palette_set = PaletteSet(_default_palette, _default_palette, _default_palette, _default_palette)
 
 
+class PaletteSetEditor(QWidget):
+    """A widget to help with editing a palette set"""
+
+    def __init__(self, parent: Optional[QWidget], palette: Optional[PaletteSet] = _default_palette_set) -> None:
+        from foundry.gui.Custom.Palette.NESPaletteSelector import ColorPickerButton
+        super().__init__(parent)
+        self.parent = parent
+        self.palette = palette
+        self.action = Observed(lambda *_: self.palette)
+        self.add_observer(lambda *_: print(self.palette))
+
+        self.palette_editors = []
+        hbox = QHBoxLayout()
+        hbox.setSizeConstraint(QLayout.SetFixedSize)
+        hbox.setSpacing(MARGIN_TIGHT)
+
+        self.background_button = ColorPickerButton.as_tiny(self, self.palette[0][0])
+        self.background_button.add_observer(lambda *_: self._set_palette_set_color(0, 0, self.background_button.color))
+        hbox.addWidget(self.background_button)
+
+        for idx in range(4):
+            editor = PaletteEditor(self, self.palette[idx])
+            editor.add_observer(lambda pal: self._set_palette_set_palette(idx, pal))
+            self.palette_editors.append(editor)
+            hbox.addWidget(editor)
+        self.setLayout(hbox)
+
+    def _set_palette_set_color(self, pal_idx, idx, color) -> None:
+        p_set = list(self.palette)
+        p = list(p_set[pal_idx])
+        p[idx] = color
+        p_set[pal_idx] = Palette(p[0], p[1], p[2], p[3])
+        self.palette = PaletteSet(p_set[0], p_set[1], p_set[2], p_set[3])
+        self.action()
+
+    def _set_palette_set_palette(self, pal_idx: int, palette: Palette):
+        p_set = list(self.palette)
+        p_set[pal_idx] = palette
+        self.palette = PaletteSet(p_set[0], p_set[1], p_set[2], p_set[3])
+
+    def add_observer(self, observer: Callable) -> None:
+        """Adds an observer to the value"""
+        self.action.attach_observer(observer)
+
+
+class PaletteEditor(QWidget):
+    """A widget to help edit a single palette"""
+    def __init__(self, parent: Optional[QWidget], palette: Optional[Palette] = _default_palette) -> None:
+        from foundry.gui.Custom.Palette.NESPaletteSelector import ColorPickerButton
+        super().__init__(parent)
+        self.parent = parent
+        self.palette = palette
+        self.action = Observed(lambda *_: self.palette)
+
+        self.buttons = []
+        hbox = QHBoxLayout()
+        hbox.setSizeConstraint(QLayout.SetFixedSize)
+        hbox.setSpacing(0)
+        for idx in range(3):
+            button = ColorPickerButton.as_tiny(self, _palette_controller.colors[idx], idx)
+            button.add_observer(lambda idx: self._set_palette_color(idx, self.buttons[idx].color))
+            self.buttons.append(button)
+            hbox.addWidget(button)
+        self.setLayout(hbox)
+
+    def _set_palette_color(self, idx, color) -> None:
+        p = list(self.palette)
+        p[idx] = color
+        self.palette = Palette(p[0], p[1], p[2], p[3])
+        self.action()
+
+    def add_observer(self, observer: Callable) -> None:
+        """Adds an observer to the value"""
+        self.action.attach_observer(observer)
+
 
 class ColorPicker(QWidget):
     """A widget to help with picking a NES color"""
