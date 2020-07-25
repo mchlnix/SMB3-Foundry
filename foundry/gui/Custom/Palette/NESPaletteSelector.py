70968228f87468791e6ac291472bed72bb78abe3
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-24 23:22:17 -0400

    Added color picker button

--- foundry/gui/Custom/Palette/NESPaletteSelector.py
+++ foundry/gui/Custom/Palette/NESPaletteSelector.py
@@ -14,6 +14,28 @@ from foundry.decorators.Observer import Observed
 
 _palette_controller = PaletteController()
 
+
+class ColorPickerButton(ColoredToolButton):
+    def __init__(self, parent: Optional[QWidget], color: Color, return_call=None):
+        super().__init__(parent, color)
+        self.action = Observed(lambda *_: self.color if self._return_call is None else self._return_call)
+        self._action.attach_observer(self.call_pop_up)
+        self._return_call = return_call
+
+    def add_observer(self, observer: Callable) -> None:
+        """Adds an observer to the value"""
+        self.action.attach_observer(observer)
+
+    def call_pop_up(self, *_):
+        """Calls on a pop up to select a new color"""
+        ColorPickerPopup(self, action=lambda value: self.on_pop_up_finish(_palette_controller.colors[value])).exec_()
+
+    def on_pop_up_finish(self, value: Color):
+        """Called when the pop up finishes"""
+        self.color = value
+        self.action()
+
+
 class ColorPickerPopup(Dialog):
     """Allows you to pick a custom color and returns the value"""
 
