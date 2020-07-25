6b1109533c4e94b4538183f37036daaf833a524c
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-25 13:53:28 -0400

    Updated size policy

--- foundry/gui/QLabel/__init__.py
+++ foundry/gui/QLabel/__init__.py
@@ -6,11 +6,14 @@ from typing import Optional
 from PySide2.QtWidgets import QWidget, QLabel, QSizePolicy
 
 
+from foundry.gui.QCore import MARGIN_TIGHT
+
+
 class Label(QLabel):
     """A generic spinner with extended functionality"""
     def __init__(self, parent: Optional[QWidget], text: str) -> None:
         super().__init__(parent=parent)
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self.setFixedHeight(self.sizeHint().toTuple()[1] + 6)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        self.setContentsMargins(0, 0, 0, 0)
         self.setText(text)
 
