fd9ba073ee18314408d5528dc316284214a08e22
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-24 23:20:21 -0400

    Added Button_Tiny

--- foundry/gui/QCore/__init__.py
+++ foundry/gui/QCore/__init__.py
@@ -8,3 +8,5 @@ LABEL_TINY = 60  # default super small size for label
 LABEL_TIGHT = 80  # default small size for label
 LABEL_MEDIUM = 100  # default medium size for a label
 LABEL_LARGE = 120  # default large size for a label
+
+BUTTON_TINY = 20  # default super small button size
