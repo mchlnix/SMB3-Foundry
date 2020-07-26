f6909c462ec8cfe19b7df99ca7e70e97f4e858d9
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 15:23:18 -0400

    Updated to use AbstractActionObject

--- foundry/gui/QPushButton/__init__.py
+++ foundry/gui/QPushButton/__init__.py
@@ -2,32 +2,28 @@
 This module includes a push button with extended functionality
 """
 
-from typing import Callable, Optional
-from PySide2.QtWidgets import QPushButton, QFormLayout, QWidget, QSizePolicy
+from typing import List, Optional
+from PySide2.QtWidgets import QPushButton, QWidget
 from PySide2.QtGui import QColor
 
-from foundry.gui.QCore import BUTTON_TINY
-from foundry.gui.QLabel import Label
-from foundry.decorators.Observer import Observed
+from foundry.gui.QCore.util import DefaultSizePartial
+from foundry.gui.QCore.Action import Action, AbstractActionObject
 
 
-class PushButton(QPushButton):
+class PushButton(QPushButton, AbstractActionObject, DefaultSizePartial):
     """A generic push button with extended functionality"""
     def __init__(self, parent: Optional[QWidget], name: Optional[str] = ""):
-        super().__init__(parent=parent, text=name)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.action = Observed(self.action)
-        self.clicked.connect(self.action)
+        QPushButton.__init__(parent, name)
+        DefaultSizePartial.__init__(self)
+        AbstractActionObject.__init__(self)
 
-    def add_observer(self, observer: Callable) -> None:
-        """Adds an observer to the value"""
-        self.action.attach_observer(observer)
-
-    def action(self, new_value: int, *_) -> int:
-        """
-        Extends the connect functionality from Qt
-        """
-        return new_value
+    def get_actions(self) -> List[Action]:
+        """Gets the actions for the object"""
+        return [
+            Action.from_signal("clicked", self.clicked, False),
+            Action.from_signal("pressed", self.pressed, False),
+            Action.from_signal("released", self.released, False)
+        ]
 
 
 class ColoredPushButton(PushButton):
@@ -35,18 +31,3 @@ class ColoredPushButton(PushButton):
     def __init__(self, parent: Optional[QWidget], color: QColor):
         super().__init__(parent=parent)
         self.setStyleSheet(f"background-color:rgb({color.red},{color.green},{color.blue})")
-
-
-class PushButtonPanel(QWidget):
-    """A push button panel with a basic form layout"""
-    def __init__(self, parent: Optional[QWidget], name: str, button: PushButton):
-        super().__init__(parent)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-
-        self.parent = parent
-        self.button = button
-        self.action = self.button.action
-        self.add_observer = self.button.add_observer
-        panel_layout = QFormLayout()
-        panel_layout.addRow(self.button, Label(self.parent, name))
-        self.setLayout(panel_layout)
