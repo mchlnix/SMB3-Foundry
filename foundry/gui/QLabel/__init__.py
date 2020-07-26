4976e247e537a49c3f8e5ed9dc548d08b34b4f83
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 12:49:22 -0400

    Updated to use DefaultSizePartial

--- foundry/gui/QLabel/__init__.py
+++ foundry/gui/QLabel/__init__.py
@@ -3,17 +3,15 @@ This module includes all the different Labels for including text
 """
 
 from typing import Optional
-from PySide2.QtWidgets import QWidget, QLabel, QSizePolicy
+from PySide2.QtWidgets import QWidget, QLabel
+
+from foundry.gui.QCore.util import DefaultSizePartial
 
 
-from foundry.gui.QCore import MARGIN_TIGHT
-
-
-class Label(QLabel):
+class Label(QLabel, DefaultSizePartial):
     """A generic spinner with extended functionality"""
     def __init__(self, parent: Optional[QWidget], text: str) -> None:
-        super().__init__(parent=parent)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.setContentsMargins(0, 0, 0, 0)
+        QLabel.__init__(self, parent)
+        DefaultSizePartial.__init__(self)
         self.setText(text)
 
