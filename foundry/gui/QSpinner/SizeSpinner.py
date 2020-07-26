e8ef7260660712ee2677a00b1708f576766c6fbd
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 15:26:08 -0400

    Updated to use new MultiSpinner

--- foundry/gui/QSpinner/SizeSpinner.py
+++ foundry/gui/QSpinner/SizeSpinner.py
@@ -5,26 +5,38 @@ action_size: An observer that returns the new Size
 the_size: Returns the current size
 """
 
-from typing import Optional, Callable
+from typing import Optional, List
 from PySide2.QtWidgets import QWidget
 
 from foundry.gui.QSpinner.MultiSpinner import MultiSpinner, SpinnerAttributes
 from foundry.game.Size import Size
 from foundry.decorators.Observer import Observed
+from foundry.gui.QCore.Action import Action
 
 
 class SizeSpinner(MultiSpinner):
     """A spinner in charge of the keeping the size"""
     def __init__(self, parent: Optional[QWidget], size: Size = Size(0, 0)) -> None:
-        super().__init__(parent, [SpinnerAttributes("Width", 0, 0xFF), SpinnerAttributes("Height", 0, 0xFF)])
+        MultiSpinner.__init__(self, parent, [SpinnerAttributes("Width", 0, 0xFF), SpinnerAttributes("Height", 0, 0xFF)])
         self.parent = parent
-        self.action_size = Observed(lambda siz: Size(siz[0], siz[1]))
-        self.the_size = size
-        self.action.attach(self.action_size)
-        self.action_size.attach(lambda siz: self.__setattr__("_size", siz))
+        self._size = size
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.parent}, {self.size})"
+
+    def _update_changed_values(self):
+        """Returns the changed values from the spinners"""
+        self.values_changed_action.observer([spinner.value() for spinner in self.spinners])
+        self._size = Size(self.spinners[0].value(), self.spinners[1].value())
+        self.size_changed_action.observer(self.the_size)
+
+    def get_actions(self) -> List[Action]:
+        """Gets the actions for the object"""
+        return [
+            Action("values_changed", Observed(lambda value: value)),
+            Action("size_changed", Observed(lambda size: size)),
+            Action("text_changed", Observed(lambda text: text)),
+        ]
 
     @property
     def the_size(self) -> Size:
@@ -34,10 +46,6 @@ class SizeSpinner(MultiSpinner):
     @the_size.setter
     def the_size(self, size: Size) -> None:
         self._size = size
-        self.spinners[0].spinner.setValue(size.width)
-        self.spinners[1].spinner.setValue(size.height)
-        self.action_size.notify(size)  # update observers
-
-    def add_observer(self, observer: Callable):
-        """Adds an observer"""
-        self.action_size.attach(observer)
+        self.spinners[0].setValue(size.width)
+        self.spinners[1].setValue(size.height)
+        self._update_changed_values()
