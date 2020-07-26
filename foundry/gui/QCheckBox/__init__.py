04e87ea44f9ddb39199a56bf997ec572edeef66e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 12:51:26 -0400

    Updated to use AbstractActionObject

--- foundry/gui/QCheckBox/__init__.py
+++ foundry/gui/QCheckBox/__init__.py
@@ -1,42 +1,22 @@
 
 
-from typing import Callable, Optional
-from PySide2.QtWidgets import QCheckBox, QFormLayout, QWidget, QSizePolicy
+from typing import List
+from PySide2.QtWidgets import QCheckBox
 
-from foundry.gui.QLabel import Label
-from foundry.decorators.Observer import Observed
-from foundry.gui.QCore import MARGIN_TIGHT
+from foundry.gui.QCore.util import DefaultSizePartial
+from foundry.gui.QCore.Action import Action, AbstractActionObject
 
 
-class CheckBox(QCheckBox):
+class CheckBox(QCheckBox, AbstractActionObject, DefaultSizePartial):
     """A generic spinner with extended functionality"""
     def __init__(self, parent, name):
-        super().__init__(parent=parent, text=name)
-        self.action = Observed(self.action)
-        self.stateChanged.connect(self.action)
-
-    def add_observer(self, observer: Callable) -> None:
-        """Adds an observer to the value"""
-        self.aciton.attach_observer(observer)
-
-    def action(self, new_value: int, *_) -> int:
-        """
-        Extends the connect functionality from Qt
-        """
-        return new_value
-
-
-class CheckboxPanel(QWidget):
-    """A spinner panel with a basic form layout"""
-    def __init__(self, parent: Optional[QWidget], name: str, checkbox: CheckBox):
-        super().__init__(parent)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-
+        QCheckBox.__init__(self, parent, name)
+        DefaultSizePartial.__init__(self)
+        AbstractActionObject.__init__(self)
         self.parent = parent
-        self.checkbox = checkbox
-        self.action = self.checkbox.action
-        self.add_observer = self.checkbox.add_observer
-        spinner_layout = QFormLayout()
-        spinner_layout.setContentsMargins(MARGIN_TIGHT, MARGIN_TIGHT, MARGIN_TIGHT, MARGIN_TIGHT)
-        spinner_layout.addRow(self.checkbox, Label(self.parent, name))
-        self.setLayout(spinner_layout)
+
+    def get_actions(self) -> List[Action]:
+        """Gets the actions for the object"""
+        return [
+            Action.from_signal("state_changed", self.stateChanged),
+        ]
