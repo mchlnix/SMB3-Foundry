a2ec83dd3d86f9010761e3159ef890ffb76adc2e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 12:47:25 -0400

    Added util

---
+++ foundry/gui/QCore/util.py
@@ -0,0 +1,22 @@
+
+
+from abc import abstractmethod
+from PySide2.QtWidgets import QSizePolicy
+
+
+def set_tight_size_policy(instance):
+    """Sets a tight size policy"""
+    try:
+        instance.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+    except AttributeError as err:
+        print(f"{err}: {instance} does not have function setSizePolicy")
+    try:
+        instance.setContentsMargins(0, 0, 0, 0)
+    except AttributeError as err:
+        print(f"{err}: {instance} does not have function setContentsMargin")
+
+
+class DefaultSizePartial:
+    """A partial class for setting the default size to the widget"""
+    def __init__(self):
+        set_tight_size_policy(self)
