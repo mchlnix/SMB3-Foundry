c24fb04b66de9668a0f80977a70a35db8aee2f10
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 12:49:06 -0400

    Updated to use AbstractActionObject

--- foundry/gui/QLineEdit/__init__.py
+++ foundry/gui/QLineEdit/__init__.py
@@ -3,50 +3,32 @@ This module contains the base functionality for spinners
 Spinner: A generic spinner with extended functionality
 """
 
-from typing import Optional
-from PySide2.QtWidgets import QWidget, QLineEdit, QSizePolicy, QFormLayout
+from typing import List
+from PySide2.QtWidgets import QLineEdit
 
-from foundry.decorators.Observer import Observed
-from foundry.gui.QCore import LABEL_TINY, MARGIN_TIGHT
-from foundry.gui.QLabel import Label
+from foundry.gui.QCore.util import DefaultSizePartial
+from foundry.gui.QCore.Action import Action, AbstractActionObject
 
 
-class LineEdit(QLineEdit):
+class LineEdit(QLineEdit, AbstractActionObject, DefaultSizePartial):
     """A generic spinner with extended functionality"""
+
     def __init__(self, parent, text):
-        super().__init__(parent)
-        self.setContentsMargins(0, 0, 0, 0)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.setText(text)
-        self.on_text_update = Observed(lambda value: value)
-        self.textEdited.connect(self.on_text_update.notify_observers)
+        QLineEdit.__init__(self, parent)
+        DefaultSizePartial.__init__(self)
+        AbstractActionObject.__init__(self)
         self.parent = parent
+
+        self.setText(text)
+
+    def get_actions(self) -> List[Action]:
+        """Gets the actions for the object"""
+        return [
+            Action.from_signal("text_edited", self.textEdited),
+            Action.from_signal("text_changed", self.textChanged),
+        ]
 
     @property
     def the_text(self):
         """Returns the text of the line editor as a property"""
         return self.text()
-
-
-class LineEditPanel(QWidget):
-    """A spinner panel with a basic form layout"""
-    def __init__(self, parent: Optional[QWidget], name: str, line_edit: LineEdit):
-        super().__init__(parent)
-        self.setContentsMargins(0, 0, 0, 0)
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-
-        self.parent = parent
-        self.line_edit = line_edit
-        self.on_text_update = self.line_edit.on_text_update
-        panel_layout = QFormLayout()
-        panel_layout.setContentsMargins(MARGIN_TIGHT, 0, MARGIN_TIGHT, 0)
-        self.label = Label(self, name)
-        panel_layout.addRow(self.label, self.line_edit)
-        self.setLayout(panel_layout)
-
-
-
-
-
-
-
