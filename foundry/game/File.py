9d684cbd9cd1294d184ef48de29bf12ff7a03d00
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-26 20:43:58 -0400

    Optimized rom write and read functions

--- foundry/game/File.py
+++ foundry/game/File.py
@@ -3,12 +3,11 @@ from typing import List, Optional
 
 from smb3parse.util.rom import Rom
 
+
+_ROM: "ROM" = None
+
+
 WORLD_COUNT = 9  # includes warp zone
-
-# W = WORLD_MAP
-# OS = OFFSET
-
-OS_SIZE = 2  # byte
 
 TSA_OS_LIST = 0x3C3F9
 TSA_TABLE_SIZE = 0x400
@@ -69,25 +68,48 @@ class Bank:
 class ROM(Rom):
     MARKER_VALUE = bytes("SMB3FOUNDRY", "ascii")
 
-    rom_data = bytearray()
+    def __new__(cls, pathname: Optional[str] = None):
+        if _ROM is not None:
+            return _ROM
+        else:
+            super().__new__(cls)
 
-    additional_data = ""
+    def __init__(self, pathname: Optional[str] = None):
+        if pathname is None:
+            raise ValueError("Rom has not been loaded")
+        self.path = pathname
+        self.name = basename(pathname)
+        self.load_from_file(pathname)
+        Rom.__init__(self, self.rom_data)
+        self._position = 0
 
-    path: str = ""
-    name: str = ""
+    @property
+    def position(self) -> int:
+        """The current index in ROM"""
+        return self._position
 
-    W_INIT_OS_LIST: List[int] = []
+    @position.setter
+    def position(self, position: int) -> None:
+        if position is None or position < 0:
+            return  # Do not want to change the position
+        if position > len(self.rom_data):
+            raise IndexError(f"Invalid position: {position} inside rom of length: {len(self.rom_data)}")
+        self._position = position
 
-    def __init__(self, path: Optional[str] = None):
-        if not ROM.rom_data:
-            if path is None:
-                raise ValueError("Rom was not loaded!")
+    def load_from_file(self, pathname: str):
+        """Loads a file from memory"""
+        with open(pathname, "rb") as rom:
+            data = bytearray(rom.read())
 
-            ROM.load_from_file(path)
+        additional_data_start = data.find(ROM.MARKER_VALUE)
 
-        super(ROM, self).__init__(ROM.rom_data)
-
-        self.position = 0
+        if additional_data_start == -1:
+            self.rom_data = data
+            self.additional_data = ""
+        else:
+            self.rom_data = data[:additional_data_start]
+            additional_data_start += len(ROM.MARKER_VALUE)
+            self.additional_data = data[additional_data_start:].decode("utf-8")
 
     @staticmethod
     def tsa_offset(object_set: int) -> int:
@@ -116,106 +138,56 @@ class ROM(Rom):
         return rom.read(tsa_start, TSA_TABLE_SIZE)
 
     @staticmethod
-    def load_from_file(path: str):
-        with open(path, "rb") as rom:
-            data = bytearray(rom.read())
-
-        ROM.path = path
-        ROM.name = basename(path)
-
-        additional_data_start = data.find(ROM.MARKER_VALUE)
-
-        if additional_data_start == -1:
-            ROM.rom_data = data
-            ROM.additional_data = ""
-        else:
-            ROM.rom_data = data[:additional_data_start]
-
-            additional_data_start += len(ROM.MARKER_VALUE)
-
-            ROM.additional_data = data[additional_data_start:].decode("utf-8")
-
-    @staticmethod
     def save_to_file(path: str):
         with open(path, "wb") as f:
-            f.write(bytearray(ROM.rom_data))
+            f.write(bytearray(_ROM.rom_data))
 
-        if ROM.additional_data:
+        if _ROM.additional_data:
             with open(path, "ab") as f:
-                f.write(ROM.MARKER_VALUE)
-                f.write(ROM.additional_data.encode("utf-8"))
+                f.write(_ROM.MARKER_VALUE)
+                f.write(_ROM.additional_data.encode("utf-8"))
 
-        ROM.path = path
-        ROM.name = basename(path)
+        _ROM.path = path
+        _ROM.name = basename(path)
 
     @staticmethod
     def set_additional_data(additional_data):
-        ROM.additional_data = additional_data
+        _ROM.additional_data = additional_data
 
-    def seek(self, position: int) -> int:
-        if position > len(ROM.rom_data) or position < 0:
-            return -1
-
+    def get_word(self, position: Optional[int] = None) -> int:
+        """Reads a word of data"""
         self.position = position
-
-        return 0
-
-    def get_word(self, position: int = -1) -> int:
-        if position >= 0:
-            k = self.seek(position) >= 0
-        else:
-            k = self.position < len(ROM.rom_data)
-
-        if k:
-            hi, lo = ROM.rom_data[self.position + 1], ROM.rom_data[self.position]
-            return_word = lo + (hi << 8)
-        else:
-            return_word = 0
-
-        self.position += 2
-
-        return return_word
-
-    def get_byte(self, position: int = -1) -> int:
-        if position >= 0:
-            k = self.seek(position) >= 0
-        else:
-            k = self.position < len(ROM.rom_data)
-
-        if k:
-            return_byte = ROM.rom_data[self.position]
-        else:
-            return_byte = 0
-
+        low_byte = self.rom_data[self.position]
         self.position += 1
+        high_byte = self.rom_data[self.position]
+        self.position += 1
+        return low_byte + (high_byte << 8)
 
-        return return_byte
+    def get_byte(self, position: Optional[int] = None) -> int:
+        """Reads a byte from rom"""
+        self.position = position
+        return self.rom_data[self.position]
 
-    def peek_byte(self, position: int = -1) -> int:
-        old_position = self.position
-
+    def peek_byte(self, position: Optional[int] = None) -> int:
+        """Gets a byte without saving the position"""
+        _position = self.position
         byte = self.get_byte(position)
-
-        self.position = old_position
-
+        self.position = _position
         return byte
 
-    def bulk_read(self, count: int, position: int = -1) -> bytearray:
-        if position >= 0:
-            self.seek(position)
-        else:
-            position = self.position
-
+    def bulk_read(self, count: int, position: Optional[int] = None) -> bytearray:
+        """Reads multiple bytes"""
+        self.position = position
+        position = self.position
         self.position += count
+        return self.rom_data[position: self.position]
 
-        return ROM.rom_data[position : position + count]
-
-    def bulk_write(self, data: bytearray, position: int = -1):
-        if position >= 0:
-            self.seek(position)
-        else:
-            position = self.position
-
+    def bulk_write(self, data: bytearray, position: Optional[int] = None) -> None:
+        """Writes multiple bytes"""
+        self.position = position
+        position = self.position
         self.position += len(data)
-
-        ROM.rom_data[position : position + len(data)] = data
+        try:
+            self.rom_data[position: position + len(data)] = data
+        except IndexError:
+            raise IndexError(f"Unable to save rom data {data} to {position}")
