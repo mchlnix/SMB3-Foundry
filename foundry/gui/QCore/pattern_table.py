536f230af28c33215f0e7ff0c02e9efa5c5bb1af
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-29 10:41:10 -0400

    Add default pattern table

---
+++ foundry/gui/QCore/pattern_table.py
@@ -0,0 +1,9 @@
+"""
+This module contains default values for the pattern table
+"""
+
+
+from foundry.game.gfx.PatternTableHandler import PatternTable
+
+
+PATTERN_TBL_DEFAULT = PatternTable(0, 0, 0x53, 0x04, 0x22, 0x32)
