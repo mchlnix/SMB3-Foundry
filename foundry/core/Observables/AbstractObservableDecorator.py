eab074bab7ae8d31b44e8e456184d2bd12591338
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-07-30 18:57:57 -0400

    Add abstract observable decorator

---
+++ foundry/core/Observables/AbstractObservableDecorator.py
@@ -0,0 +1,25 @@
+
+
+from abc import ABC
+from typing import Callable
+
+from .AbstractObservable import AbstractObservable
+
+
+class AbstractObservableDecorator(AbstractObservable, ABC):
+    """
+    The interface for decorating a function to be an observable
+    observables: A group of callables that will be notified of the result
+    notify_observers: Notifies the observables
+    attach_observer: Adds a callable to the dict of observables
+    delete_observer: Removes a callable to the dict of observables
+    """
+    function: Callable
+
+    def __init__(self, function: Callable):
+        super(AbstractObservableDecorator, self).__init__()
+        self.function = function
+
+    def __call__(self, *args, **kwargs) -> None:
+        result = self.function(*args, **kwargs)
+        super(AbstractObservableDecorator, self).__call__(result)
