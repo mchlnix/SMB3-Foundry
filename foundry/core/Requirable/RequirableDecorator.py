e527bc29886631ef59d7abdf94bc47beaa94cea6
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-01 11:08:40 -0400

    Refactor requirable to use new classes

--- foundry/core/Requirable/RequirableDecorator.py
+++ foundry/core/Requirable/RequirableDecorator.py
@@ -2,33 +2,21 @@
 Provides a class decorator to help with requiring other methods
 """
 
-from typing import Callable, Tuple
+from typing import Tuple
+
+from .AbstractRequirableDecorator import AbstractRequirableDecorator
+from .Requirable import Requirable
 
 
-class Requirable:
-    """A class that is observable"""
-    def __init__(self, function):
-        self.function = function
-        self.notify_required = self.determine_if_safe
-        self.attach_required = self.attach_requirement
-        self.observers = []
-
-    def __call__(self, *args, **kwargs):
-        if self.determine_if_safe():
-            result = self.function(*args, **kwargs)
-            return result
-        return False
-
-    def determine_if_safe(self) -> bool:
-        """Notifies every observer and determines if it is safe to continue"""
-        for observer in self.observers:
-            if not observer():
-                return False
-        return True
-
-    def attach_requirement(self, observer: Callable, *_) -> None:
-        """Adds an observer"""
-        self.observers.append(observer)
+class RequirableDecorator(AbstractRequirableDecorator, Requirable):
+    """
+    The implementation for a generic requirable decorator
+    function: The function being decorated
+    requirements: A group of callables that will determine if we should run the function
+    determine_if_safe: Returns if it is safe
+    attach_requirement: Adds another requirement to be tested
+    delete_requirement: Removes a requirement
+    """
 
 
 class SmartRequirable(Requirable):
