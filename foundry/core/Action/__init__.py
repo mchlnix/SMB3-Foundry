35b18f5754351c44fd00dc2d916b5e4303064492
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-01 13:53:16 -0400

    Reformat add doc string

--- foundry/core/Action/__init__.py
+++ foundry/core/Action/__init__.py
@@ -1,3 +1,9 @@
+"""
+This module contains general functions for actions and action objects
+has_actions: Determines if an object has actions or not
+"""
+
+
 def has_actions(action_object):
     """Raises an error if the action object does not have _actions"""
     if not hasattr(action_object, "_actions"):
