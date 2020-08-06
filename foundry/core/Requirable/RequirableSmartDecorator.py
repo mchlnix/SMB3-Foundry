813493a96b89f41abc55fecd65f9e1b4e51d69a3
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-06 13:29:24 -0400

    Update smart decorator to new system

--- foundry/core/Requirable/RequirableSmartDecorator.py
+++ foundry/core/Requirable/RequirableSmartDecorator.py
@@ -1,6 +1,8 @@
 from typing import Tuple
+from inspect import signature
 
 from foundry.core.Requirable.RequirableDecorator import RequirableDecorator
+from foundry.core.Requirable.Requirable import _logger
 
 
 class SmartRequirableDecorator(RequirableDecorator):
@@ -14,8 +16,23 @@ class SmartRequirableDecorator(RequirableDecorator):
 
     def determine_if_safe(self) -> Tuple[bool, str, str]:
         """Notifies every observer and determines if it is safe to continue"""
-        for requirement in self.requirements:
-            safe, reason, additional_info = requirement()
-            if not safe:
-                return False, reason, additional_info
-        return True, '', ''
+        _logger.debug(f"{self} inquiring {self.requirements.values()}")
+        value, reason, additional_info = True, "", ""
+
+        keys_to_remove = []
+        for key, requirement in self.requirements.items():
+            try:
+                safe, reason, additional_info = requirement()
+                if not safe:
+                    return safe, reason, additional_info
+            except NameError:
+                _logger.debug(f"Removed deleted observer with identification {key}")
+                keys_to_remove.append(key)  # the observer no longer exists
+            except TypeError:
+                raise TypeError(f"{requirement.__name__}{str(signature(requirement))} failed to execute")
+
+        for key in keys_to_remove:
+            #  remove any observer that no longer exists
+            self.delete_requirement(key)
+
+        return value, reason, additional_info
