575351221cc10ad05f504cdd41f8d18880549cb7
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-06 17:41:30 -0400

    Remove unused file

--- foundry/gui/QMenus/SettingsMenuElement.py
+++
@@ -1,6 +0,0 @@
-"""
-This module includes the MenuElementSettings, which provides an interface to easily add boolean settings to the menu
-MenuActionSettings: The menu action in charge of settings
-"""
-
-
