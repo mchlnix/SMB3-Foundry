7e5238e20ed400f8b58d9191f639f9d43af5f570
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-06 14:35:07 -0400

    Remove uneeded imports

--- foundry/gui/QMenus/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The container for all the local file menu functions
-"""
-
-from foundry.gui.QMenus.Menu.Menu import Menu
-from foundry.gui.QMenus.MenuElement.AbstractMenuElementSafe import AbstractMenuElementSafe
-from foundry.gui.QMenus.MenuElement.AbstractMenuElement import AbstractMenuElement
-
-
