a490ada09f6e12f71405717a1d1864076982332c
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-06 14:07:02 -0400

    Move MenuElementOpen to AbstractMenuElementOpen

--- foundry/gui/QMenus/FileMenu.py
+++ foundry/gui/QMenus/FileMenu.py
@@ -17,7 +17,8 @@ SaveASM6MenuElement: An element to save ASM6 files
 """
 
 from foundry.gui.SettingsDialog import show_settings
-from . import Menu, AbstractMenuElement, MenuElementOpen, MenuElementSave
+from . import Menu, AbstractMenuElement, MenuElementSave
+from .MenuElement.AbstractMenuElementOpen import MenuElementOpen
 
 
 ROM_FILE_FILTER = "ROM files (*.nes *.rom);;All files (*)"
--- foundry/gui/QMenus/__init__.py
+++ foundry/gui/QMenus/__init__.py
@@ -65,27 +65,6 @@ class MenuAction(QAction):
         return self.value
 
 
-class MenuElementOpen(AbstractMenuElementSafe, ABC):
-    """A Menu Element that contains tests required for opening"""
-    def __init__(self, parent, add_action: bool = True) -> None:
-        self.action = ObservedAndRequired(self.action)
-        self.open = ObservableDecorator(lambda path: path)
-        super().__init__(parent, add_action)
-
-    def action(self):
-        """Routine for handling the entire opening process"""
-        path = self.path
-        if not path:
-            return False
-
-        try:
-            self.open(path)
-        except IOError as exp:
-            QMessageBox.warning(self.parent, type(exp).__name__, f"Cannot open file '{path}'.")
-            return False
-        return True
-
-
 class MenuElementSave(AbstractMenuElementSafe, ABC):
     """A Menu Element that contains tests required for saving"""
     def __init__(self, parent, add_action: bool = True) -> None:
