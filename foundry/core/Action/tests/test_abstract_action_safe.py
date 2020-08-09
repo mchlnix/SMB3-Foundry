cf334de7cbd2ab50046bfa0ee0f8b689ec6c3e9b
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-09 14:51:39 -0400

    Move ActionSafeTest to ActionSafeTest

--- foundry/core/Action/tests/test_abstract_action_safe.py
+++ foundry/core/Action/tests/test_abstract_action_safe.py
@@ -1,26 +1,9 @@
-
-
-from typing import Callable
-
-from foundry.core.Action.AbstractActionSafe import AbstractActionSafe
+from foundry.core.Action.tests.ActionSafeTest import ActionSafeTest
 from foundry.core.Requirable.RequirableSmartDecorator import SmartRequirableDecorator
 
 
 TEST_REASON = "test"
 TEST_ADDITIONAL = "additional info"
-
-
-class ActionSafeTest(AbstractActionSafe):
-    """A concrete implementation of ActionSafe used for testing"""
-    def __init__(self, name: str, func: Callable, proceed: bool = False) -> None:
-        AbstractActionSafe.__init__(self, name, func)
-        self.proceed = proceed
-        self.reason, self.additional_info = None, None
-
-    def proceed_message(self, reason: str, additional_information: str) -> bool:
-        """THe proceed message"""
-        self.reason, self.additional_info = reason, additional_information
-        return self.proceed
 
 
 class ChangingWarningTest:
