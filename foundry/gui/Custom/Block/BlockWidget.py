dd6cc5d3909a0029f5696ef1d22ee2155776190e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-10-22 19:04:20 -0400

    Update BlockWidget to use AbstractBlock

--- foundry/gui/Custom/Block/BlockWidget.py
+++ foundry/gui/Custom/Block/BlockWidget.py
@@ -14,7 +14,6 @@ from foundry.core.Action.AbstractActionObject import AbstractActionObject
 
 from foundry.gui.QWidget import Widget
 
-from foundry.game.File import ROM, TSA_TABLE_SIZE, TSA_TABLE_INTERVAL
 from foundry.game.gfx.PatternTableHandler import PatternTableHandler
 from foundry.game.gfx.Palette import PaletteSet
 from foundry.game.gfx.drawable.Block import Block as MetaBlock
@@ -36,23 +35,12 @@ class BlockWidget(Widget, AbstractActionObject):
             self,
             parent: Optional[QWidget],
             name: str,
-            size: Optional[Size],
-            index: int,
-            ptn_tbl: PatternTableHandler,
-            pal_set: PaletteSet,
-            tsa_offset: int,
-            transparency: bool = True
+            block: AbstractBlock
     ) -> None:
         Widget.__init__(self, parent)
         AbstractActionObject.__init__(self)
         self.name = name
-        self.size = size if size else Size(1, 1)
-        self.index = index
-        self.pattern_table = ptn_tbl
-        self.palette_set = pal_set
-        self.tsa_offset = tsa_offset
-        self._tsa_data = None
-        self.transparency = transparency
+        self.block = block
 
         self._initialize_internal_observers()
 
@@ -63,59 +51,46 @@ class BlockWidget(Widget, AbstractActionObject):
     @property
     def size(self) -> Size:
         """The size of the block in units of 16 pixels"""
-        return self._size
+        return self.block.size
 
     @size.setter
     def size(self, size: Size) -> None:
-        self._size = size
+        self.block.size = size
         self.size_update_action(self._size)
 
     @property
     def tsa_data(self) -> bytearray:
         """Find the tsa data from a given offset"""
-        if not self._tsa_data:
-            return ROM().bulk_read(TSA_TABLE_SIZE, (self.tsa_offset * TSA_TABLE_INTERVAL) + 0x10)
-        else:
-            return self._tsa_data
-
-    @property
-    def tsa_offset(self) -> int:
-        """The offset in banks to the current tsa"""
-        return self._tsa_offset
-
-    @tsa_offset.setter
-    def tsa_offset(self, offset: int) -> None:
-        self._tsa_offset = offset
-        self.refresh_event_action()
+        return self.block.tsa_data
 
     @property
     def pattern_table(self) -> PatternTableHandler:
         """The pattern table for the tiles"""
-        return self._pattern_table
+        return self.block.pattern_table
 
     @pattern_table.setter
     def pattern_table(self, pattern_table: PatternTableHandler) -> None:
-        self._pattern_table = pattern_table
+        self.block.pattern_table = pattern_table
         self.refresh_event_action()
 
     @property
     def palette_set(self) -> PaletteSet:
         """The palette currently used by the tsa"""
-        return self._palette_set
+        return self.block.palette_set
 
     @palette_set.setter
     def palette_set(self, palette_set: PaletteSet) -> None:
-        self._palette_set = palette_set
+        self.block.palette_set = palette_set
         self.refresh_event_action()
 
     @property
     def transparency(self) -> bool:
         """Determines if the blocks will be transparent"""
-        return self._transparency
+        return self.block.transparency
 
     @transparency.setter
     def transparency(self, transparency: bool) -> None:
-        self._transparency = transparency
+        self.block.transparency = transparency
         self.refresh_event_action()
 
     def get_actions(self) -> List[Action]:
@@ -136,13 +111,5 @@ class BlockWidget(Widget, AbstractActionObject):
     def paintEvent(self, event: QPaintEvent) -> None:
         """Paints the widget"""
         painter = QPainter(self)
-
-        block = MetaBlock.from_rom(self.index, self.palette_set, self.pattern_table, self.tsa_data)
-        block.draw(
-            painter,
-            Position(0, 0),
-            Size(MetaBlock.image_length * self.size.width, MetaBlock.image_height * self.size.width),
-            transparent=self.transparency
-        )
-
+        self.block.draw(painter, Position(0, 0))
         super().paintEvent(event)
