ee25d5fb8e78855ab5145b78ead23f822aebc686
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-24 00:34:49 -0500

    Refactor logger to seperate file

--- foundry/core/Observables/Observable.py
+++ foundry/core/Observables/Observable.py
@@ -4,20 +4,11 @@ This serves as a generic implementation of the AbstractObserver
 """
 
 import random
-import logging
 from inspect import signature
-from foundry import log_dir
 from typing import Callable, Any, Dict, Optional, Hashable
 
 from .AbstractObservable import AbstractObservable
-
-
-_logger = logging.getLogger(__name__)
-_logger.setLevel(logging.WARNING)
-_formatter = logging.Formatter('%(levelname)s: %(message)s | %(asctime)s | %(name)s')
-_handler = logging.FileHandler(f"{log_dir}/observable.log")
-_handler.setFormatter(_formatter)
-_logger.addHandler(_handler)
+from ..logs.observables import logger
 
 
 class Observable(AbstractObservable):
@@ -32,20 +23,20 @@ class Observable(AbstractObservable):
 
     def __init__(self, name: str = None):
         super(Observable, self).__init__(name)
-        _logger.debug(f"{self} was created")
+        logger.debug(f"{self} was created")
 
     def notify_observers(self, result: Any) -> None:
         """Update all the observers"""
-        _logger.debug(f"{str(self)} notified {self.observers.values()} result {result}")
+        logger.debug(f"{str(self)} notified {self.observers.values()} result {result}")
         keys_to_remove = []
         for key, observer in self.observers.items():
             try:
                 observer(result)
             except NameError:
-                _logger.debug(f"Removed deleted observer with identification {key}")
+                logger.debug(f"Removed deleted observer with identification {key}")
                 keys_to_remove.append(key)  # the observer no longer exists
             except TypeError as err:
-                _logger.error(f"{err}: {observer.__name__}{str(signature(observer))} received {result}")
+                logger.error(f"{err}: {observer.__name__}{str(signature(observer))} received {result}")
                 raise TypeError(f"{observer.__name__}{str(signature(observer))} received {result}")
 
         for key in keys_to_remove:
@@ -59,13 +50,13 @@ class Observable(AbstractObservable):
             if temp_id not in self.observers:
                 identifier = temp_id
 
-        _logger.debug(f"{str(self)} attached {observer} with key {identifier} to {self.observers}")
+        logger.debug(f"{str(self)} attached {observer} with key {identifier} to {self.observers}")
         self.observers.update({identifier: observer})
 
     def delete_observable(self, identifier: Hashable) -> None:
         """Removes an observer"""
         try:
             del self.observers[identifier]
-            _logger.debug(f"{identifier} was deleted from {self.observers}")
+            logger.debug(f"{identifier} was deleted from {self.observers}")
         except KeyError:
-            _logger.warning(f"Failed to delete {identifier} from {self.observers}")
+            logger.warning(f"Failed to delete {identifier} from {self.observers}")
