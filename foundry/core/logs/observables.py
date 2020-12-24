1d7ed031d6f64f9f41a61adbc0650d5207b7a2fa
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-24 01:03:07 -0500

    Update formatter to be easier to read

--- foundry/core/logs/observables.py
+++ foundry/core/logs/observables.py
@@ -5,7 +5,7 @@ from foundry import log_dir
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
-formatter = logging.Formatter('%(levelname)s: %(message)s | %(asctime)s | %(name)s')
+formatter = logging.Formatter('%(levelname)s: %(message)s')
 handler = logging.FileHandler(f"{log_dir}/observable.log")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
