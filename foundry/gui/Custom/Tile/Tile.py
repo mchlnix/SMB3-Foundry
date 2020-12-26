ddafe8d5f9dd782db6171e40167ff0dbdc35d8c1
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-26 07:38:37 -0500

    Create Tile.py

---
+++ foundry/gui/Custom/Tile/Tile.py
@@ -0,0 +1,59 @@
+"""
+This module includes the AbstractBlock
+BlockWidget: An abstract representation of what a block is.  Unlike gfx/Block this is meant to retain state.
+"""
+
+from foundry.game.gfx.PatternTableHandler import PatternTableHandler
+from foundry.game.gfx.Palette import Palette
+from foundry.core.geometry.Size.Size import Size
+
+from .AbstractTile import AbstractTile
+
+
+class Tile(AbstractTile):
+    """A generic implementation of a Tile"""
+
+    @property
+    def size(self) -> Size:
+        """The size of the block in units of 16 pixels"""
+        return self._size
+
+    @size.setter
+    def size(self, size: Size) -> None:
+        self._size = size
+
+    @property
+    def index(self) -> int:
+        """The index of the block"""
+        return self._index
+
+    @index.setter
+    def index(self, index: int) -> None:
+        self._index = index
+
+    @property
+    def pattern_table(self) -> PatternTableHandler:
+        """The pattern table for the tiles"""
+        return self._pattern_table
+
+    @pattern_table.setter
+    def pattern_table(self, pattern_table: PatternTableHandler) -> None:
+        self._pattern_table = pattern_table
+
+    @property
+    def palette(self) -> Palette:
+        """The palette currently used by the tsa"""
+        return self._palette
+
+    @palette.setter
+    def palette_set(self, palette: Palette) -> None:
+        self._palette = palette
+
+    @property
+    def transparency(self) -> bool:
+        """Determines if the blocks will be transparent"""
+        return self._transparency
+
+    @transparency.setter
+    def transparency(self, transparency: bool) -> None:
+        self._transparency = transparency
