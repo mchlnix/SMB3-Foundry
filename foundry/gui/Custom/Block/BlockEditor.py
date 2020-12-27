9472df3afde3bd54ffd62772ea0b7da30cef1150
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-12-27 05:56:19 -0500

    Remove meta_block
    The meta_block is not needed as the widget will already store an instance of the block.  Thus, if we just use one instance, the design flow will be much simpler and more efficient.

--- foundry/gui/Custom/Block/BlockEditor.py
+++ foundry/gui/Custom/Block/BlockEditor.py
@@ -16,7 +16,6 @@ from foundry.gui.Custom.Block.BlockTileTrackableObject import BlockTileTrackable
 from foundry.gui.Custom.Block.AbstractBlock import AbstractBlock
 from foundry.gui.QSpinner.HexSpinner import HexSpinner
 from foundry.gui.QWidget import Widget
-from foundry.gui.Custom.BlockPattern.BlockPattern import BlockPattern
 
 
 class BlockEditor(Widget, AbstractActionObject):
@@ -31,28 +30,13 @@ class BlockEditor(Widget, AbstractActionObject):
         Widget.__init__(self, parent)
         AbstractActionObject.__init__(self)
 
-        self.meta_block = block
-        print(self.meta_block)
-        self._block_pattern = BlockPattern.from_tsa_data(self.meta_block.index, self.meta_block.tsa_data)
-
-        self._set_up_layout()
+        self._set_up_layout(block)
         self._initialize_internal_observers()
 
-        self.index = self.meta_block.index  # Update the spinners
+        self.index = block.index  # Update the spinners
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.parent}, {self.spinners})"
-
-    @property
-    def block_pattern(self) -> BlockPattern:
-        """The pattern of the current block"""
-        return self._block_pattern
-
-    @block_pattern.setter
-    def block_pattern(self, pattern: BlockPattern) -> None:
-        self._block_pattern = pattern
-        self._push_block_update()
-        self.block_changed_action(self.tsa_data)
 
     @property
     def index(self) -> int:
@@ -61,55 +45,54 @@ class BlockEditor(Widget, AbstractActionObject):
 
     @index.setter
     def index(self, index: int) -> None:
-        self.meta_block.index = index
-        self.block_pattern = BlockPattern.from_tsa_data(self.block.index, self.block.tsa_data)
+        self.block.index = index
 
     @property
     def tsa_data(self) -> bytearray:
         """"The pattern table of the block"""
-        return self.meta_block.tsa_data
+        return self.block.tsa_data
 
     @tsa_data.setter
     def tsa_data(self, tsa_data: bytearray) -> None:
-        self.meta_block.tsa_data = tsa_data
+        self.block.tsa_data = tsa_data
 
     @property
     def size(self) -> Size:
         """The size of the block in units of 16 pixels"""
-        return self.meta_block.size
+        return self.block.size
 
     @size.setter
     def size(self, size: Size) -> None:
-        self.meta_block.size = size
+        self.block.size = size
 
     @property
     def pattern_table(self) -> PatternTableHandler:
         """The pattern table for the tiles"""
-        return self.meta_block.pattern_table
+        return self.block.pattern_table
 
     @pattern_table.setter
     def pattern_table(self, pattern_table: PatternTableHandler) -> None:
-        self.meta_block.pattern_table = pattern_table
+        self.block.pattern_table = pattern_table
 
     @property
     def palette_set(self) -> PaletteSet:
         """The palette currently used by the tsa"""
-        return self.palette_set
+        return self.block.palette_set
 
     @palette_set.setter
     def palette_set(self, palette_set: PaletteSet) -> None:
-        self.meta_block.palette_set = palette_set
+        self.block.palette_set = palette_set
 
     @property
     def transparency(self) -> bool:
         """Determines if the blocks will be transparent"""
-        return self.meta_block.transparency
+        return self.block.transparency
 
     @transparency.setter
     def transparency(self, transparency: bool) -> None:
-        self.meta_block.transparency = transparency
+        self.block.transparency = transparency
 
-    def _set_up_layout(self) -> None:
+    def _set_up_layout(self, block: AbstractBlock) -> None:
         """Returns the widgets layout"""
         grid = QGridLayout()
         grid.setContentsMargins(0, 0, 0, 0)
@@ -120,7 +103,7 @@ class BlockEditor(Widget, AbstractActionObject):
         top_right_spinner = HexSpinner(self, maximum=0xFF)
         grid.addWidget(top_right_spinner, 0, 2)
 
-        self.block = BlockTileTrackableObject(self, "Block", self.meta_block)
+        self.block = BlockTileTrackableObject(self, "Block", block)
         grid.addWidget(self.block, 0, 1, 0, 1, Qt.AlignCenter)
 
         bottom_left_spinner = HexSpinner(self, maximum=0xFF)
@@ -135,7 +118,6 @@ class BlockEditor(Widget, AbstractActionObject):
     def _initialize_internal_observers(self) -> None:
         """Initializes internal observers for special events"""
         name = self.__class__.__name__
-        block_name = self.block.__class__.__name__
 
         def update_block_pattern_closure(idx):
             """Updates the block pattern"""
@@ -156,36 +138,6 @@ class BlockEditor(Widget, AbstractActionObject):
         self.block.size_update_action.observer.attach_observer(
             lambda size: self.size_update_action(size), name=f"{name} Size Updated"
         )
-
-        self.block.palette_set_update_action.observer.attach_observer(
-            lambda palette_set: setattr(self, "palette_set", palette_set),
-            name=f"{block_name} Push Palette Set to {name}"
-        )
-        self.block.pattern_table_update_action.observer.attach_observer(
-            lambda pattern_table: setattr(self, "pattern_table", pattern_table),
-            name=f"{block_name} Push Pattern Table to {name}"
-        )
-        self.block.tsa_data_update_action.observer.attach_observer(
-            lambda tsa_data: setattr(self, "tsa_data", tsa_data),
-            name=f"{block_name} Push TSA Data to {name}"
-        )
-        self.block.index_update_action.observer.attach_observer(
-            lambda index: setattr(self, "index", index),
-            name=f"{block_name} Push Index to {name}"
-        )
-
-        self.block_changed_action.observer.attach_observer(
-            lambda *_: self._push_meta_block_to_gui(),
-            name=f"{name} Push Meta Block Data to {block_name}"
-        )
-
-    def _push_meta_block_to_gui(self) -> None:
-        """Pushes the meta block to the gui"""
-        self.block.index = self.meta_block.index
-        self.block.palette = self.meta_block.palette_set
-        self.block.pattern_table = self.meta_block.pattern_table
-        self.block.tsa_data = self.meta_block.tsa_data
-        self.block.size = self.meta_block.size
 
     def _push_block_update(self) -> None:
         """Pushes an update to the block"""
