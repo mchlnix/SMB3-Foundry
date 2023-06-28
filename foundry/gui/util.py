325c33016c742653af6ede2c8afdcdcf792ee42d
Michael Nix <mchl.nix@googlemail.com>
2023-06-28 15:38:40 +0900

    adds grouper function from more_itertools

--- foundry/gui/util.py
+++ foundry/gui/util.py
@@ -1,4 +1,4 @@
-from itertools import filterfalse, tee
+from itertools import filterfalse, tee, zip_longest
 
 from PySide6.QtCore import QPoint
 from PySide6.QtWidgets import QApplication, QWidget
@@ -22,3 +22,19 @@ def partition(pred, iterable):
     # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
     t1, t2 = tee(iterable)
     return filterfalse(pred, t1), filter(pred, t2)
+
+
+def grouper(iterable, n, *, incomplete="fill", fillvalue=None):
+    """Collect data into non-overlapping fixed-length chunks or blocks"""
+    # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
+    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
+    # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
+    args = [iter(iterable)] * n
+    if incomplete == "fill":
+        return zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == "strict":
+        return zip(*args, strict=True)
+    if incomplete == "ignore":
+        return zip(*args)
+    else:
+        raise ValueError("Expected fill, strict, or ignore")
